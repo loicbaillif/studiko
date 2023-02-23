706b0bffb1bb948b3dac22769f92ebac8cd0096c
loicbaillif <larmedeciel@gmail.com>
2023-02-23 07:18:52 +0100

    Kotlin/begin/overview/ex1/v2 Second proposal

--- src/main/kotlin/overview/Exercise1.kt
+++ src/main/kotlin/overview/Exercise1.kt
@@ -3,6 +3,5 @@ package overview
 fun Exercise1() {
     // https://hyperskill.org/learn/step/18840
     println("Overview - Exercise 1 : Multiple Statements")
-    println("Learn ")
-    println("Kotlin")
+    println("Learn\nKotlin")
 }
