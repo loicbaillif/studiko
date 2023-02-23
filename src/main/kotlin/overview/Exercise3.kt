ac599bf97c5d3d32a926a99a56743ba11c88ed80
loicbaillif <larmedeciel@gmail.com>
2023-02-23 12:55:00 +0100

    Kotlin/begin/overview/e3/v1 First proposal

--- src/main/kotlin/overview/Exercise3.kt
+++ src/main/kotlin/overview/Exercise3.kt
@@ -7,5 +7,5 @@ fun exercise3() {
      * Overview - Exercise: Fix the code
      */
     println("Overview - Exercise 3 : Fix this line")
-    println(2 + 2 = 4) // fix this line
+    println("2 + 2 = 4") // fix this line
 }
