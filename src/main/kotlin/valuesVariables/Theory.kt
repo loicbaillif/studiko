aced3f7e1e23e97c8dd0b1f28d9d51496448e2ea
loicbaillif <larmedeciel@gmail.com>
2023-02-24 22:27:23 +0100

    Kotlin/begin/valvar/th/v3 const keyword

--- src/main/kotlin/valuesVariables/Theory.kt
+++ src/main/kotlin/valuesVariables/Theory.kt
@@ -6,4 +6,5 @@ fun theory() {
     println("1) Declaration")
     println("\tval to declare a read-only variable")
     println("\tvar to declare a mutable variable")
+    println("\tconst to add as a prefix, for variables known at compile time")
 }
