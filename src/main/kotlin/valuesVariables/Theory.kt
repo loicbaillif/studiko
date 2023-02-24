d0ddfad99e9c4dd402abd20123059a3acfd8d72c
loicbaillif <larmedeciel@gmail.com>
2023-02-24 22:23:26 +0100

    Kotlin/begin/valvar/th/v2 Declaration

--- src/main/kotlin/valuesVariables/Theory.kt
+++ src/main/kotlin/valuesVariables/Theory.kt
@@ -1,5 +1,9 @@
 package valuesVariables
 
 fun theory() {
+    // https://hyperskill.org/learn/step/4371
     println("***** Theory *****")
+    println("1) Declaration")
+    println("\tval to declare a read-only variable")
+    println("\tvar to declare a mutable variable")
 }
