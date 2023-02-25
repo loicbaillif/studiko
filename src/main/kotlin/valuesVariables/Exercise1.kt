07701c4220b1e279a571fe92cd1c8f3bd7a6d521
loicbaillif <larmedeciel@gmail.com>
2023-02-25 21:59:41 +0100

    Kotlin/begin/valvar/ex1/v1 Initial proposal

--- src/main/kotlin/valuesVariables/Exercise1.kt
+++ src/main/kotlin/valuesVariables/Exercise1.kt
@@ -7,4 +7,10 @@ fun exercise1() {
      * Values and Variables - Exercise : Fix declarations
      */
     println("Exercise 1 : Fix declarations")
+
+    // Fix declaration. Do not touch the lines above
+    a = 512343
+    b = 3431231
+    // Do not touch the lines below
+    println("$a $b")
 }
