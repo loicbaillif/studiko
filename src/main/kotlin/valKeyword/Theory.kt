3e159c5fb4d5bf70f90978df3c77b134c8c97b7c
loicbaillif <larmedeciel@gmail.com>
2023-02-26 11:22:23 +0100

    Kotlin/begin/val/th/v1 First declarations

--- src/main/kotlin/valKeyword/Theory.kt
+++ src/main/kotlin/valKeyword/Theory.kt
@@ -4,7 +4,11 @@ fun theory() {
     // https://hyperskill.org/learn/step/17408
     println("***** Theory *****")
 
-
+    println("*** val variables***")
+    val pi = 3.1415
+    val greetingsMsg = "Hello and Welcome"
+    println(pi)
+    println(greetingsMsg)
 
     println("***** End of Theory *****")
 }
