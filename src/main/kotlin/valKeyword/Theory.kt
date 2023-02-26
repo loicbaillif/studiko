d434b1a85f17e2d1a192a8c37d5bbbb39b7ce7dc
loicbaillif <larmedeciel@gmail.com>
2023-02-26 11:45:48 +0100

    Kotlin/begin/val/th/v5 mutating a val ?

--- src/main/kotlin/valKeyword/Theory.kt
+++ src/main/kotlin/valKeyword/Theory.kt
@@ -4,7 +4,7 @@ fun theory() {
     // https://hyperskill.org/learn/step/17408
     println("***** Theory *****")
 
-    println("*** val variables***")
+    println("\n*** 1) val variables***")
     val pi = 3.1415
     val greetingsMsg = "Hello and Welcome"
     println(pi)
@@ -22,5 +22,12 @@ fun theory() {
     println(someInt)
     println(anyFloat)
 
-    println("***** End of Theory *****")
+
+    println("\n*** 2) val Variables and mutability ***")
+    // Create a new val
+    val myMutableList = mutableListOf(1, 2, 3, 4, 5)
+    // Update the previous val with another value, won't work
+    // myMutableList = mutableListOf(1, 2, 3, 4, 5, 6) // Forbidden
+
+    println("\n\n***** End of Theory *****")
 }
