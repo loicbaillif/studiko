61d6d5f88375f1f31a503fcd0d37a1039b00db4a
loicbaillif <larmedeciel@gmail.com>
2023-02-27 21:56:22 +0100

    Kotlin/begin/dataTypes/v1 first declaration

--- src/main/kotlin/typesDataStruct/dataTypes/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/Theory.kt
@@ -4,7 +4,8 @@ fun theory() {
     // https://hyperskill.org/learn/step/4388
     println("***** Theory *****")
 
-
+    println("1) Variable types")
+    val integer1 = 32
 
     println("\n\n***** End of Theory *****")
 }
