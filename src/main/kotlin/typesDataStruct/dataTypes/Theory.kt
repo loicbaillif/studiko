8f69411c119a20f21fe5fb5ab1ad6570864533c2
loicbaillif <larmedeciel@gmail.com>
2023-03-02 19:19:56 +0100

    Kotlin/begin/dataTypes/v4 Type mismatch - init

--- src/main/kotlin/typesDataStruct/dataTypes/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/Theory.kt
@@ -4,7 +4,7 @@ fun theory() {
     // https://hyperskill.org/learn/step/4388
     println("***** Theory *****")
 
-    println("1) Variable types")
+    println("\n***1) Variable types")
     val integer1 = 32
     val integer2: Int = 55
     println("First integer = $integer1")
@@ -13,6 +13,8 @@ fun theory() {
     val nInt: Int = 111_111
     println("My text = $myText")
     println("My int = $nInt")
+
+    println("\n***2) Type mismatch")
 
 
     println("\n\n***** End of Theory *****")
