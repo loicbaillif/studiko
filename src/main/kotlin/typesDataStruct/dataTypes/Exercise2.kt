b768ab84619f48369649d5483c11e0c525a709a7
loicbaillif <larmedeciel@gmail.com>
2023-03-03 22:00:15 +0100

    Kotlin/begin/dataTypes/ex2/init Edit it

---
+++ src/main/kotlin/typesDataStruct/dataTypes/Exercise2.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.dataTypes
+
+fun exercise2() {
+    // https://hyperskill.org/learn/step/16077
+    // Exercise 2 : Edit it
+
+    val var: String = "Kotlin "
+    val part2 = is a cross-platform
+    String val = ", statically typed, "
+    val part4 = "general-purpose programming language with type coercion."
+
+    print(part1)
+    print(part2)
+    print(part3)
+    print(part4)
+}
--- src/main/kotlin/typesDataStruct/dataTypes/Index.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/Index.kt
@@ -3,5 +3,6 @@ package typesDataStruct.dataTypes
 fun index() {
     println("********** Data Types **********\n")
     // theory()
-    exercise1()
+    // exercise1()
+    exercise2()
 }
