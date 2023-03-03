b8de4c465896e11a04045b752bc88cbdd5ea4272
loicbaillif <larmedeciel@gmail.com>
2023-03-03 21:20:56 +0100

    Kotlin/begin/dataTypes/ex1/v2 Version 2 : treat as Int

--- src/main/kotlin/typesDataStruct/dataTypes/Exercise1.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/Exercise1.kt
@@ -4,6 +4,6 @@ fun exercise1() {
     // https://hyperskill.org/learn/step/4614
     // Exercise 1 : Fix the program
 
-    val number: String = "100"
+    val number: Int = 100
     println(number)
 }
