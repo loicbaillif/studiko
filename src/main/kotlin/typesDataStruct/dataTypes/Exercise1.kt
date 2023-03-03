52283c2e122f0020d7e7f4bfb04cd73f62e5f4f0
loicbaillif <larmedeciel@gmail.com>
2023-03-03 21:17:28 +0100

    Kotlin/begin/dataTypes/ex1/init Fix the program - init

---
+++ src/main/kotlin/typesDataStruct/dataTypes/Exercise1.kt
@@ -0,0 +1,9 @@
+package typesDataStruct.dataTypes
+
+fun exercise1() {
+    // https://hyperskill.org/learn/step/4614
+    // Exercise 1 : Fix the program
+
+    val number: String = 100
+    println(number)
+}
--- src/main/kotlin/typesDataStruct/dataTypes/Index.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/Index.kt
@@ -2,5 +2,6 @@ package typesDataStruct.dataTypes
 
 fun index() {
     println("********** Data Types **********\n")
-    theory()
+    // theory()
+    exercise1()
 }
