34724b0cb3a6034ada615369130e3b04610a4ae5
loicbaillif <larmedeciel@gmail.com>
2023-03-04 22:23:30 +0100

    Kotlin/begin/invFun/th/v3 function producing a value

--- src/main/kotlin/ctrlflow/func/invokFunc/Theory.kt
+++ src/main/kotlin/ctrlflow/func/invokFunc/Theory.kt
@@ -7,6 +7,13 @@ fun theory() {
     println("1) Functions arguments")
     println("println(\"ping\") ==> function with & argument")
 
+    println("2) Producing a result")
+    val someInt = -10
+    val somePositiveInt = Math.abs(someInt)
+    println("a positive int : $somePositiveInt")
+    val someResult = println("Here I am")
+    println("result from a println = $someResult")
+
 
     println("\n\n***** End of Theory *****")
 }
