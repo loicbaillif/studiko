94dd06efe9cbe4172ef980d8b2dcce7f79abe7e6
loicbaillif <larmedeciel@gmail.com>
2023-03-04 16:16:22 +0100

    Kotlin/begin/stdOut/ex2/v1 First version

--- src/main/kotlin/basics/stdinout/stdOutput/Exercise2.kt
+++ src/main/kotlin/basics/stdinout/stdOutput/Exercise2.kt
@@ -5,6 +5,16 @@ fun exercise2() {
     // Exercise 2: From 0 to 9
     println("*** Exercise 2: From 0 to 9 ***")
 
+    println("0")
+    println("1")
+    println("2")
+    println("3")
+    println("4")
+    println("5")
+    println("6")
+    println("7")
+    println("8")
+    println("9")
 
 
     println("*** End of exercise ***")
