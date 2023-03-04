24985bd3bf8b9ec980cb8d0de8b8baa54a315143
loicbaillif <larmedeciel@gmail.com>
2023-03-04 16:25:56 +0100

    Kotlin/begin/stdOut/ex3/init Correct the code

---
+++ src/main/kotlin/basics/stdinout/stdOutput/Exercise3.kt
@@ -0,0 +1,16 @@
+package basics.stdinout.stdOutput
+
+fun exercise3() {
+    // https://hyperskill.org/learn/step/15360
+    // Correct the code
+
+    println("*** Exercise: Correct the code ***")
+
+    System.out.printf(1, 2, 3...)
+    printLine("Line print here ")
+    lnPrint(42)
+    printLn("Pay attention to")
+    println("syntax")
+
+    println("*** End of exercise ***")
+}
