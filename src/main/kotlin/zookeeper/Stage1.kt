67b500ebb4696ff259dc7c6d6bf70031a4d8ab37
loicbaillif <larmedeciel@gmail.com>
2023-03-05 11:36:06 +0100

    Kotlin/begin/zookeper/st1/v2 Print all text - end of stage 1

--- src/main/kotlin/zookeeper/Stage1.kt
+++ src/main/kotlin/zookeeper/Stage1.kt
@@ -2,4 +2,9 @@ package zookeeper
 
 fun stage1() {
     println("Stage 1: Rush into print")
+    println("I love animals!")
+    println("Let's check on the animals...")
+    println("The deer looks fine.")
+    println("The bat looks happy.")
+    println("The lion looks healthy.")
 }
