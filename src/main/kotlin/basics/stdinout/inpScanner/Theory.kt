b68822653e72a1841e3238e1823d45687440cfd4
loicbaillif <larmedeciel@gmail.com>
2023-03-05 19:26:15 +0100

    Kotlin/begin/inpScanner/th/v1 Create and import scanner

--- src/main/kotlin/basics/stdinout/inpScanner/Theory.kt
+++ src/main/kotlin/basics/stdinout/inpScanner/Theory.kt
@@ -1,9 +1,14 @@
 package basics.stdinout.inpScanner
 
+import java.util.Scanner
+
 fun theory() {
+    // https://hyperskill.org/learn/step/4445
     println("***** Theory *****")
 
-
+    println("*** 1) What is Java Scanner")
+    println("Need to be imported")
+    val scanner = Scanner(System.`in`)
 
     println("\n\n***** End of Theory *****")
 }
