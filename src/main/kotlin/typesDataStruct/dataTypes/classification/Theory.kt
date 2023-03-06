2b0649aeda4f00b346c27d19cb644906b046ee85
loicbaillif <larmedeciel@gmail.com>
2023-03-06 21:08:37 +0100

    Kotlin/begin/classif/th/v4 Numbers - some examples

--- src/main/kotlin/typesDataStruct/dataTypes/classification/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/classification/Theory.kt
@@ -7,6 +7,13 @@ fun theory() {
     println("1) Numbers")
     println("Four types:")
     println("\t. Byte\n\t. Short\n\t. Int\n\t. Long")
+    val zero = 0 // Int
+    val one = 1 // Int
+    val oneMillion = 1_000_000 // Int as well
+    println("Examples:")
+    println("\t. zero = $zero")
+    println("\t. one = $one")
+    println("\t. one million = $oneMillion")
 
 
 
