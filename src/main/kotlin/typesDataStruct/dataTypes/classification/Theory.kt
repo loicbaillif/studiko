2b3abe3fbe4fbd36893872bde63c78171a43735c
loicbaillif <larmedeciel@gmail.com>
2023-03-06 21:05:53 +0100

    Kotlin/begin/classif/th/v2 Numbers - the 4 types

--- src/main/kotlin/typesDataStruct/dataTypes/classification/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/classification/Theory.kt
@@ -4,6 +4,10 @@ fun theory() {
     // https://hyperskill.org/learn/step/4455
     println("***** Theory *****")
 
+    println("1) Numbers")
+    println("Four types:")
+    println("\t. Byte\n\t. Short\n\t. Int\n\t. Long")
+
 
 
     println("\n\n***** End of Theory *****")
