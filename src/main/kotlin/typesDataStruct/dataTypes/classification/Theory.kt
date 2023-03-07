8db4a270061083dce91eacb15ece1d6db9b59d3d
loicbaillif <larmedeciel@gmail.com>
2023-03-07 08:46:40 +0100

    Kotlin/begin/classif/th/v7 Numbers - last examples

--- src/main/kotlin/typesDataStruct/dataTypes/classification/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/classification/Theory.kt
@@ -11,14 +11,16 @@ fun theory() {
     val one = 1 // Int
     val oneMillion = 1_000_000 // Int as well
     println("Examples:")
-    println("\t. zero = $zero")
-    println("\t. one = $one")
-    println("\t. one million = $oneMillion")
+    println("\t. zero: Value = $zero - Type = ${zero::class.java.typeName}")
+    println("\t. one: Value = $one - Type = ${one::class.java.typeName}")
+    print("\t. one million: Value = $oneMillion - ")
+    println("Type = ${oneMillion::class.java.typeName}")
     val twoMillions = 2_000_000L // Long because tagged with L
     val bigNumber = 1_000_000_000_000_000 // Long because too big for Int
     val ten: Long = 10 // Long because explicitely specified
     println("\nOther examples:")
-    println("\t. two millions: Value = $twoMillions - Type = ${twoMillions::class.java.typeName}")
+    print("\t. two millions: Value = $twoMillions - ")
+    println("Type = ${twoMillions::class.java.typeName}")
 
 
 
