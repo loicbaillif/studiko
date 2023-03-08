856a88f72fc129caf610019e400e04fbc27e907d
loicbaillif <larmedeciel@gmail.com>
2023-03-08 21:24:25 +0100

    Kotlin/begin/classif/th/v8 float and double - examples

--- src/main/kotlin/typesDataStruct/dataTypes/classification/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/classification/Theory.kt
@@ -17,10 +17,20 @@ fun theory() {
     println("Type = ${oneMillion::class.java.typeName}")
     val twoMillions = 2_000_000L // Long because tagged with L
     val bigNumber = 1_000_000_000_000_000 // Long because too big for Int
-    val ten: Long = 10 // Long because explicitely specified
+    val ten: Long = 10 // Long because explicitly specified
     println("\nOther examples:")
     print("\t. two millions: Value = $twoMillions - ")
     println("Type = ${twoMillions::class.java.typeName}")
+    print("\t. some big number: Value = $bigNumber - ")
+    println("Type = ${bigNumber::class.java.typeName}")
+    print("\t. 10: Value = $ten - Type = ${ten::class.java.typeName}")
+    val piFive = 3.1415
+    val eFloat = 2.71828f
+    val someFloat: Float = 13.57f
+    println("\t. pi: Value = $piFive - Type = ${piFive::class.java.typeName}")
+    println("\t. e: Value = $eFloat - Type = ${eFloat::class.java.typeName}")
+    print("\t. some float: Value = $someFloat - ")
+    println("Type = ${someFloat::class.java.typeName}")
 
 
 
