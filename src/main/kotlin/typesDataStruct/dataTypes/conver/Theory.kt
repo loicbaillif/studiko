87a6cfdfa5a911b88743478cd64e0f19392a344c
loicbaillif <larmedeciel@gmail.com>
2023-03-10 07:13:14 +0100

    Kotlin/begin/conversion/th/init Theory - init

--- src/main/kotlin/typesDataStruct/dataTypes/conver/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/conver/Theory.kt
@@ -3,6 +3,8 @@ package typesDataStruct.dataTypes.conver
 fun theory() {
     // https://hyperskill.org/learn/step/4672
     println("***** Theory *****")
+    println("1) Conversion between numeric types")
+
 
 
 
