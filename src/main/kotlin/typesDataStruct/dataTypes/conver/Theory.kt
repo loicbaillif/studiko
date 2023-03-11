8f75e5c4c458ffce15e12c406905b964b235b6c0
loicbaillif <larmedeciel@gmail.com>
2023-03-11 14:57:45 +0100

    Kotlin/begin/conversion/th/v1 First example

--- src/main/kotlin/typesDataStruct/dataTypes/conver/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/conver/Theory.kt
@@ -1,10 +1,16 @@
 package typesDataStruct.dataTypes.conver
 
+import kotlin.math.sqrt
+
 fun theory() {
     // https://hyperskill.org/learn/step/4672
     println("***** Theory *****")
     println("1) Conversion between numeric types")
-
+    val someInt1: Int = 169
+    val someResult1: Double = sqrt(someInt1.toDouble())
+    println("\t. Number: Value = $someInt1 - Type = ${someInt1::class.java.typeName}")
+    print("\t. sqrt(Number): Value = $someResult1 - ")
+    println(someResult1::class.java.typeName)
 
 
 
