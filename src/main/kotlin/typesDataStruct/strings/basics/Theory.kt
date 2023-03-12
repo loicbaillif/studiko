5c9cd3cad14179af0be0d2ff6b5507d254fec724
loicbaillif <larmedeciel@gmail.com>
2023-03-12 21:26:54 +0100

    Kotlin/begin/stringBasics/th/v1 The length of a String

--- src/main/kotlin/typesDataStruct/strings/basics/Theory.kt
+++ src/main/kotlin/typesDataStruct/strings/basics/Theory.kt
@@ -3,6 +3,11 @@ package typesDataStruct.strings.basics
 fun theory() {
     // https://hyperskill.org/learn/step/12553
     println("***** Theory *****")
+    println("1) The length of a String")
+    val someString1 = "I learn Kotlin"
+    val someString2 = ""
+    println("String: Value = $someString1 - length = ${someString1.length}")
+    println("String: Value = $someString2 - length = ${someString2.length}")
 
 
 
