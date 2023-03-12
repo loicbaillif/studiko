395dfa2a3588135505d27bc3be99c44daf22975d
loicbaillif <larmedeciel@gmail.com>
2023-03-12 21:39:50 +0100

    Kotlin/begin/stringBasics/th/v3 Concatenating Strings: 2nd example

--- src/main/kotlin/typesDataStruct/strings/basics/Theory.kt
+++ src/main/kotlin/typesDataStruct/strings/basics/Theory.kt
@@ -9,12 +9,17 @@ fun theory() {
     println("\t. String: Value = $someString1 - length = ${someString1.length}")
     println("\t. String: Value = $someString2 - length = ${someString2.length}")
 
-    println("2) Concatening Strings")
+    println("2) Concatenating Strings")
     val someString3 = "abc"
     val someString4 = "def"
     val someString5 = someString3 + someString4
     println("\t. String1: Value = $someString3\n\t. String2: Value = $someString4")
-    println("\t. String1 + String2 = $someString5")
+    println("\t. String1 + String2 = $someString5\n")
+    val someString6 = "John"
+    val someString7 = "Black"
+    val someString8 = someString6 + " " + someString7
+    println("\t. String1: Value = $someString6\n\t. String2: Value = $someString7")
+    println("\t. String1 + \" \" + String2 = $someString8")
 
 
 
