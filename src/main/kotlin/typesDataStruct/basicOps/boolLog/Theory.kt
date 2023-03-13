cad2f9da7036e63ddfbb41f8741f46b60c48c752
loicbaillif <larmedeciel@gmail.com>
2023-03-13 21:04:33 +0100

    Kotlin/begin/boolLogic/th/v3 operator NOT

--- src/main/kotlin/typesDataStruct/basicOps/boolLog/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/boolLog/Theory.kt
@@ -4,14 +4,14 @@ fun theory() {
     // https://hyperskill.org/learn/step/4515
     println("***** Theory *****")
 
-    println("\n1) Boolean variables")
+    println("\n\n1) Boolean variables")
     val someTrue = true
     val someFalse = false
     println("Two possible values:")
     println("\t. $someTrue")
     println("\t. $someFalse")
 
-    println("\n2) Reading Boolean values")
+    println("\n\n2) Reading Boolean values")
     val someString1 = "true"
     val someString2 = "TRUE"
     val someString3 = "truE"
@@ -24,6 +24,13 @@ fun theory() {
     println("\t. String: $someString4 - toBoolean() => ${someString4.toBoolean()}")
     println("\t. String: $someString5 - toBoolean() => ${someString5.toBoolean()}")
     println("\t. String: $someString6 - toBoolean() => ${someString6.toBoolean()}")
+
+    println("\n\n3) Logical Operators")
+    val boolFalse = false
+    val boolTrue = true
+    println("==> NOT : !")
+    println("\t. !$boolFalse = ${!boolFalse}")
+    println("\t. !$boolTrue = ${!boolTrue}")
 
 
 
