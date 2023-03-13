303958bea59c9abe6062a882a59d6cf18809ab81
loicbaillif <larmedeciel@gmail.com>
2023-03-13 19:16:46 +0100

    Kotlin/begin/boolLogic/th/v1 2 possible values

--- src/main/kotlin/typesDataStruct/basicOps/boolLog/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/boolLog/Theory.kt
@@ -4,6 +4,12 @@ fun theory() {
     // https://hyperskill.org/learn/step/4515
     println("***** Theory *****")
 
+    println("\n1) Boolean variables")
+    val someTrue = true
+    val someFalse = false
+    println("Two possible values:")
+    println("\t. $someTrue")
+    println("\t. $someFalse")
 
 
 
