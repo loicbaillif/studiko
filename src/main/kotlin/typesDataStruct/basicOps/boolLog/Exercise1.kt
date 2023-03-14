2ce384f29a84a90ebea84f524ed0245d0e0e26a7
loicbaillif <larmedeciel@gmail.com>
2023-03-14 07:18:07 +0100

    Kotlin/begin/boolLogic/ex1/v1 get variables

--- src/main/kotlin/typesDataStruct/basicOps/boolLog/Exercise1.kt
+++ src/main/kotlin/typesDataStruct/basicOps/boolLog/Exercise1.kt
@@ -5,6 +5,9 @@ fun exercise1() {
     // Exercise 1: Program a boolean function
 
     println("*** Exercise: Program a boolean function ***")
+    val x = readLine().toBoolean()
+    val y = readLine().toBoolean()
+    val z = readLine().toBoolean()
 
 
     println("End of exercise")
