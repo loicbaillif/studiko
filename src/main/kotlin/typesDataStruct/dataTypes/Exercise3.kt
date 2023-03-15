77a27d3f0a425b773095e92a84e9615259e3c4e6
loicbaillif <larmedeciel@gmail.com>
2023-03-15 20:17:54 +0100

    Kotlin/begin/dataTypes/ex3/v1 Int max value

--- src/main/kotlin/typesDataStruct/dataTypes/Exercise3.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/Exercise3.kt
@@ -4,6 +4,7 @@ fun exercise3() {
     // https://hyperskill.org/learn/step/4689
     // Max values of integer types
     println("***** Exercise: Max values of integer types *****")
+    println(Int.MAX_VALUE)
 
     println("\n\n***** End of exercise *****")
 }
