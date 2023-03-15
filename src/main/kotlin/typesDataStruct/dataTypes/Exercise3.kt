f66af7d3ffe6bba9ecd43a8e83cd6434f31a1880
loicbaillif <larmedeciel@gmail.com>
2023-03-15 20:20:12 +0100

    Kotlin/begin/dataTypes/ex3/v4 Byte max value

--- src/main/kotlin/typesDataStruct/dataTypes/Exercise3.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/Exercise3.kt
@@ -4,6 +4,7 @@ fun exercise3() {
     // https://hyperskill.org/learn/step/4689
     // Max values of integer types
     println("***** Exercise: Max values of integer types *****")
+    println(Byte.MAX_VALUE)
     println(Short.MAX_VALUE)
     println(Int.MAX_VALUE)
     println(Long.MAX_VALUE)
