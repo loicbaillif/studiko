c1dce1485645643d086bd296dfc3b73fa86c90a8
loicbaillif <larmedeciel@gmail.com>
2023-03-16 21:22:16 +0100

    Kotlin/begin/objects/th/v2 Object method example

--- src/main/kotlin/typesDataStruct/variab/objects/Theory.kt
+++ src/main/kotlin/typesDataStruct/variab/objects/Theory.kt
@@ -4,11 +4,14 @@ fun theory() {
     // https://hyperskill.org/learn/step/11233
     println("***** Theory *****")
 
-    println("1) State and Behaviour")
+    println("\n1) State and Behaviour")
     val someString1 = "Hi"
     // Object property example
     println("\t. myString: $someString1")
     println("\t. myString.length = ${someString1.length}")
+    // Object method example
+    println("\n\t. myString: $someString1")
+    println("\t. myString.repeat(3) = ${someString1.repeat(3)}")
 
 
     println("\n\n***** End of Theory *****")
