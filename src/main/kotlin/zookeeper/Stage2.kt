5448a2548f7c8ac32b8028a448b2d15ab428f7cc
loicbaillif <larmedeciel@gmail.com>
2023-03-17 06:44:26 +0100

    Kotlin/begin/zookeeper/st2/v1 First proposal

--- src/main/kotlin/zookeeper/Stage2.kt
+++ src/main/kotlin/zookeeper/Stage2.kt
@@ -3,6 +3,28 @@ package zookeeper
 fun stage2() {
     println("\n*** Stage 2 : Show me an animal! ***")
 
+    val camel = """
+    Switching on the camera in the camel habitat...
+     ___.-''''-.
+    /___  @    |
+    ',,,,.     |         _.'''''''._
+         '     |        /           \
+         |     \    _.-'             \
+         |      '.-'                  '-.
+         |                               ',
+         |                                '',
+          ',,-,                           ':;
+               ',,| ;,,                 ,' ;;
+                  ! ; !'',,,',',,,,'!  ;   ;:
+                 : ;  ! !       ! ! ;  ;   :;
+                 ; ;   ! !      ! !  ; ;   ;,
+                ; ;    ! !     ! !   ; ;
+                ; ;    ! !    ! !     ; ;
+               ;,,      !,!   !,!     ;,;
+               /_I      L_I   L_I     /_I
+    Look at that! Our little camel is sunbathing!""".trimIndent()
+
+    println(camel)
 
 
     println("\n*** End of Stage 2 ***")
