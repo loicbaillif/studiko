6ec498d076c6deb7a3757c5d0f59f64331effdec
loicbaillif <larmedeciel@gmail.com>
2023-03-18 22:53:03 +0100

    Kotlin/begin/readln/th/v4 toDouble() example

--- src/main/kotlin/basics/stdinout/readlnFun/Theory.kt
+++ src/main/kotlin/basics/stdinout/readlnFun/Theory.kt
@@ -4,7 +4,7 @@ fun theory() {
     // https://hyperskill.org/learn/step/11224
     println("***** Theory *****")
 
-    println("1) Using readln()")
+    println("\n1) Using readln()")
     println("!!!!! readLine()!! to be used for old versions of Kotlin !!!!!")
     println("Since version 1.6, use readln()")
     /*
@@ -12,14 +12,20 @@ fun theory() {
     println("You entered: \n\t $someString1")
      */
 
-    println("2) toInt() and toLong()")
-    println("Enter any number:")
+    println("\n2) toInt() and toLong()")
+    // println("Enter any number:")
     /*
     val someInt1 = readln().toInt()
     println("You entered: \n\t Type: ${someInt1::class.java.typeName} - $someInt1")
-    */
     val someLong1 = readln().toLong()
     println("You entered: \n\t Type: ${someLong1::class.java.typeName} - $someLong1")
+     */
+
+    println("\n3) toDouble() and toBoolean()")
+    println("Enter any floating type number")
+    val someDouble1 = readln().toDouble()
+    println("You entered:\n\t. Type: ${someDouble1::class.java.typeName}")
+    println("\t. Value: $someDouble1")
 
 
 
