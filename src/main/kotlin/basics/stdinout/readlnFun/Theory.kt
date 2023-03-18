f06b33f44099df3e7dec5499dd95d6cbb37b7f71
loicbaillif <larmedeciel@gmail.com>
2023-03-18 22:45:43 +0100

    Kotlin/begin/readln/th/v2 toInt() example

--- src/main/kotlin/basics/stdinout/readlnFun/Theory.kt
+++ src/main/kotlin/basics/stdinout/readlnFun/Theory.kt
@@ -4,13 +4,16 @@ fun theory() {
     // https://hyperskill.org/learn/step/11224
     println("***** Theory *****")
 
-    println("2) Using readln")
+    println("1) Using readln()")
     println("!!!!! readLine()!! to be used for old versions of Kotlin !!!!!")
     println("Since version 1.6, use readln()")
     val someString1 = readln()
     println("You entered: \n\t $someString1")
 
-
+    println("2) toInt() and toLong()")
+    println("Enter any number:")
+    val someInt1 = readln().toInt()
+    println("You entered: \n\t Type: ${someInt1::class.java.typeName} - $someInt1")
 
 
     println("\n\n***** End of Theory *****")
