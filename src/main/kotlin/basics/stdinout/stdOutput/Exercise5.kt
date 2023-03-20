925f75106d2695873b58fe3e2b2db848f40fcbab
loicbaillif <larmedeciel@gmail.com>
2023-03-20 22:09:41 +0100

    Kotlin/begin/stdOut/ex5/init Print a triangle

---
+++ src/main/kotlin/basics/stdinout/stdOutput/Exercise5.kt
@@ -0,0 +1,11 @@
+package basics.stdinout.stdOutput
+
+fun exercise5() {
+    // https://hyperskill.org/learn/step/15361
+    // Print a triangle
+    println("*** Exercise: Print a triangle ***")
+
+
+
+    println("\n\n*** End of Exercise ***")
+}
