f23e3e51f860a8fcb5352a2a0abafa69abf908e0
loicbaillif <larmedeciel@gmail.com>
2023-03-25 10:52:35 +0100

    Kotlin/begin/readln/ex2/init Fix the input

---
+++ src/main/kotlin/basics/stdinout/readlnFun/Exercise2.kt
@@ -0,0 +1,11 @@
+package basics.stdinout.readlnFun
+
+fun exercise2() {
+    // https://hyperskill.org/learn/step/11263
+    // Fix the input
+
+    println("*** Exercise: Fix the input ***")
+
+
+    println("End of exercise")
+}
--- src/main/kotlin/basics/stdinout/readlnFun/Index.kt
+++ src/main/kotlin/basics/stdinout/readlnFun/Index.kt
@@ -3,5 +3,6 @@ package basics.stdinout.readlnFun
 fun index() {
     println("********** Reading data with a readln **********\n")
     // theory()
-    exercise1()
+    // exercise1()
+    exercise2()
 }
