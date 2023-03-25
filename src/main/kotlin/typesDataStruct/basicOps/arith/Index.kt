6835d6c4151358af5486f6eec5f0ed66a2165cd8
loicbaillif <larmedeciel@gmail.com>
2023-03-25 11:42:33 +0100

    Kotlin/begin/arith/th/init Theory

--- src/main/kotlin/typesDataStruct/basicOps/arith/Index.kt
+++ src/main/kotlin/typesDataStruct/basicOps/arith/Index.kt
@@ -2,5 +2,6 @@ package typesDataStruct.basicOps.arith
 
 fun index() {
     println("********** Arithmetic operations **********\n")
+    theory()
 
 }
---
+++ src/main/kotlin/typesDataStruct/basicOps/arith/Theory.kt
@@ -0,0 +1,9 @@
+package typesDataStruct.basicOps.arith
+
+fun theory() {
+    // https://hyperskill.org/learn/step/4472
+    println("***** Theory *****")
+
+
+    println("\n\n***** End of theory *****")
+}
