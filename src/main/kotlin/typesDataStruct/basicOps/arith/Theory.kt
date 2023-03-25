f17b0365e4aa38c9817c94232fa9c9f345881cec
loicbaillif <larmedeciel@gmail.com>
2023-03-25 11:44:52 +0100

    Kotlin/begin/arith/th/v1 Binary operators: addition

--- src/main/kotlin/typesDataStruct/basicOps/arith/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/arith/Theory.kt
@@ -4,6 +4,10 @@ fun theory() {
     // https://hyperskill.org/learn/step/4472
     println("***** Theory *****")
 
+    println("\n1) Binary Operators")
+    println("\t. Addition")
+    println("\t\t24 + 31 = 55\n\t\t32 + 10 = 42")
+
 
     println("\n\n***** End of theory *****")
 }
