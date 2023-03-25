337f3e77d9814635cde09871e2f7d635ddb409aa
loicbaillif <larmedeciel@gmail.com>
2023-03-25 10:05:10 +0100

    Kotlin/begin/readln/ex1/init Read a double - init

---
+++ src/main/kotlin/basics/stdinout/readlnFun/Exercise1.kt
@@ -0,0 +1,11 @@
+package basics.stdinout.readlnFun
+
+fun exercise1() {
+    // https://hyperskill.org/learn/step/11264
+    // Read a Double
+
+    println("*** Exercise: Read a Double ***")
+
+
+    println("End of exercise")
+}
