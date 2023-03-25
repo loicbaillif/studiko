79e276683f483675bd8d3f1474ee57e5fb087c5e
loicbaillif <larmedeciel@gmail.com>
2023-03-25 11:53:42 +0100

    Kotlin/begin/arith/th/v3 Binary operators: multiplication

--- src/main/kotlin/typesDataStruct/basicOps/arith/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/arith/Theory.kt
@@ -9,6 +9,9 @@ fun theory() {
     println("\t\t24 + 31 = 55\n\t\t32 + 10 = 42")
     println("\t. Subtraction")
     println("\t\t40 - 20 = 20\n\t\t20 - 40 = -20")
+    println("\t. Multiplication")
+    println("\t\t3 * 33 = 99\n\t\t21 * 12 = 252")
+
 
 
     println("\n\n***** End of theory *****")
