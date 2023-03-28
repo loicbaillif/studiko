761918bd182c9a8e148b2f436fdd7e24787ed781
loicbaillif <larmedeciel@gmail.com>
2023-03-28 14:23:13 +0200

    Kotlin/begin/typeNumEx/th/v2 automatic long casting

--- src/main/kotlin/typesDataStruct/dataTypes/typeNumEx/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/typeNumEx/Theory.kt
@@ -6,8 +6,11 @@ fun theory() {
     println("\n1) Type coercion - examples")
     val someInt: Int = 100
     val someLong: Long = 1000
-    println("Value: $someInt - type = ${someInt::class.java.typeName}")
-    println("Value: $someLong - type = ${someLong::class.java.typeName}")
+    println("\t. Value: $someInt - type: ${someInt::class.java.typeName}")
+    println("\t. Value: $someLong - type: ${someLong::class.java.typeName}")
+    val result1 = someInt + someLong
+    print("\t. Value: $someInt + $someLong = $result1 - ")
+    println("type: ${result1::class.java.typeName}")
 
     println("\n\n***** End of theory *****")
 }
