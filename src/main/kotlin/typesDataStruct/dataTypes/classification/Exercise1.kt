054e6a9446641e5b8247eac3c5e09fdfb2549c5c
loicbaillif <larmedeciel@gmail.com>
2023-03-28 13:38:40 +0200

    Kotlin/begin/classif/ex1/v4 Condensed version

--- src/main/kotlin/typesDataStruct/dataTypes/classification/Exercise1.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/classification/Exercise1.kt
@@ -5,9 +5,7 @@ fun exercise1() {
     // Size, min and max
     println("*** Exercise : Size, min and max ***")
 
-    println(Int.SIZE_BITS) // size
-    println(Int.MIN_VALUE) // min
-    println(Int.MAX_VALUE) // max
+    println("${Int.SIZE_BITS}\n${Int.MIN_VALUE}\n${Int.MAX_VALUE}")
 
     println("*** End of exercise ***")
 }
