4896d19792096cd8120a0890a5c6a9d98e017d6d
loicbaillif <larmedeciel@gmail.com>
2023-03-28 14:18:54 +0200

    Kotlin/begin/typeNumEx/th/v1 int and long variables declaration

--- src/main/kotlin/typesDataStruct/dataTypes/typeNumEx/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/typeNumEx/Theory.kt
@@ -3,5 +3,11 @@ package typesDataStruct.dataTypes.typeNumEx
 fun theory() {
     println("***** Theory *****")
 
+    println("\n1) Type coercion - examples")
+    val someInt: Int = 100
+    val someLong: Long = 1000
+    println("Value: $someInt - type = ${someInt::class.java.typeName}")
+    println("Value: $someLong - type = ${someLong::class.java.typeName}")
+
     println("\n\n***** End of theory *****")
 }
