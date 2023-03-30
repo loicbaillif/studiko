9dcc716c00bc45276d1e63cfe30d02daff74e2ef
loicbaillif <larmedeciel@gmail.com>
2023-03-30 11:29:19 +0200

    Kotlin/begin/incDec/th/v1 Operator +=

--- src/main/kotlin/typesDataStruct/basicOps/incDec/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/incDec/Theory.kt
@@ -3,6 +3,11 @@ package typesDataStruct.basicOps.incDec
 fun theory() {
     println("***** Theory *****")
 
+    println("1) Assignment Operations")
+    var a = 4
+    println("\t. a = $a")
+    a += 3
+    println("\t. a += 3 ==> $a")
 
 
     println("\n\n***** End of theory *****")
