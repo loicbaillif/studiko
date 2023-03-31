908c730c5876fd8e1810cff1f0a519382d247880
loicbaillif <larmedeciel@gmail.com>
2023-03-31 11:06:38 +0200

    Kotlin/begin/intAct/th/v1 Calculus with int

--- src/main/kotlin/typesDataStruct/basicOps/intAction/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/intAction/Theory.kt
@@ -4,6 +4,17 @@ fun theory() {
     // https://hyperskill.org/learn/step/4488
     println("***** Theory *****")
 
+    println("\n1) Int and long integer types")
+    val someInt1 = 3
+    val someInt2 = 7
+    val result1 = someInt1 + someInt2
+    val result2 = result1 * someInt1
+    val result3 = result2 % someInt2
+    println("\nSome calculus with integers:")
+    println("\t. $someInt1 + $someInt2 = $result1")
+    println("\t. $result1 * $someInt1 = $result2")
+    println("\t. $result2 / $someInt2 = $result3")
+
 
 
     println("\n\n***** End of theory *****")
