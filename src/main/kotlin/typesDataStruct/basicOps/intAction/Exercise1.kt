573792d14a6917684c15a7de16ee5c5c14527c74
loicbaillif <larmedeciel@gmail.com>
2023-03-31 20:13:28 +0200

    Kotlin/begin/intAct/ex1/v4 Final version, condensed

--- src/main/kotlin/typesDataStruct/basicOps/intAction/Exercise1.kt
+++ src/main/kotlin/typesDataStruct/basicOps/intAction/Exercise1.kt
@@ -9,11 +9,10 @@ fun exercise1() {
     val studentsClass2 = readln().toInt()
     val studentsClass3 = readln().toInt()
 
-    val desksClass1 = studentsClass1 / 2 + studentsClass1 % 2
-    val desksClass2 = studentsClass2 / 2 + studentsClass2 % 2
-    val desksClass3 = studentsClass3 / 2 + studentsClass3 % 2
-
-    val result = desksClass1 + desksClass2 + desksClass3
+    val result =
+        studentsClass1 / 2 + studentsClass1 % 2 +
+        studentsClass2 / 2 + studentsClass2 % 2 +
+        studentsClass3 / 2 + studentsClass3 % 2
     println(result)
 
     println("*** End of exercise ***")
