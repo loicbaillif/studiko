390c302669a1646fb4d8654e5cbdf4ecd5e2b37b
loicbaillif <larmedeciel@gmail.com>
2023-04-01 13:51:46 +0200

    Kotlin/begin/relOps/ex1/v3 comparison value turned in const - final version

--- src/main/kotlin/typesDataStruct/basicOps/relOps/Exercise1.kt
+++ src/main/kotlin/typesDataStruct/basicOps/relOps/Exercise1.kt
@@ -1,10 +1,10 @@
 package typesDataStruct.basicOps.relOps
 
+const val lowLimit = 10
 fun exercise1() {
     // https://hyperskill.org/learn/step/4531
     // Who is less than 10?
     println("\n*** Exercise: Who is less than 10? ***")
-    val lowLimit = 10
     val userInput = readln().toInt()
     println(userInput < lowLimit)
 
