bf379e97f4973de0d4df9ed2a67a40d9d67f3d61
loicbaillif <larmedeciel@gmail.com>
2023-04-01 13:16:52 +0200

    Kotlin/begin/relOps/th/v2 list of relational operators, part2

--- src/main/kotlin/typesDataStruct/basicOps/relOps/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/relOps/Theory.kt
@@ -9,6 +9,9 @@ fun theory() {
     println("\t. != \t To check non-equality")
     println("\t. > \t To check strictly greater than")
     println("\t. < \t To check strictly lower than")
+    println("\t. >= \t To check greater than or equal")
+    println("\t. <= \t To check lower than or equal")
+    println("It always result in a Boolean value")
 
 
 
