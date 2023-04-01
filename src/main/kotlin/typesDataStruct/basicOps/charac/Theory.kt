8452d02e389573f0c552f84d3778d8bfb0edd1fc
loicbaillif <larmedeciel@gmail.com>
2023-04-01 15:33:15 +0200

    Kotlin/begin/charac/th/v1 First examples

--- src/main/kotlin/typesDataStruct/basicOps/charac/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/charac/Theory.kt
@@ -4,6 +4,12 @@ fun theory() {
     // https://hyperskill.org/learn/step/4680
     println("***** Theory *****")
 
+    println("1) What is a character")
+    val someChar1 : Char = 'G'
+    val someChar2 = 'o'
+    val someChar3 = ' '
+    val someChar4 = '€'
+    println("Characters: $someChar1 - $someChar2 - $someChar3 - $someChar4")
 
     println("\n\n***** End of theory *****")
 }
