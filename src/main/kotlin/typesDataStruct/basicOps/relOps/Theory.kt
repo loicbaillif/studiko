ed573cf5ada12dca4a54e280faaca141a9e03f29
loicbaillif <larmedeciel@gmail.com>
2023-04-01 13:15:36 +0200

    Kotlin/begin/relOps/th/v1 list of relational operators, part1

--- src/main/kotlin/typesDataStruct/basicOps/relOps/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/relOps/Theory.kt
@@ -4,6 +4,11 @@ fun theory() {
     // https://hyperskill.org/learn/step/4525
     println("***** Theory *****")
 
+    println("\n1) List of relational operators")
+    println("\t. == \t To check equality")
+    println("\t. != \t To check non-equality")
+    println("\t. > \t To check strictly greater than")
+    println("\t. < \t To check strictly lower than")
 
 
 
