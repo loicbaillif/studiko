227ee602a9214ffe7bff99344858f978d6f1ba0f
loicbaillif <larmedeciel@gmail.com>
2023-04-02 21:52:04 +0200

    Kotlin/begin/charac/ex1/v5 Refactor; temp val removed

--- src/main/kotlin/typesDataStruct/basicOps/charac/Exercise1.kt
+++ src/main/kotlin/typesDataStruct/basicOps/charac/Exercise1.kt
@@ -4,14 +4,10 @@ fun exercise1() {
     // https://hyperskill.org/learn/step/12308
     // Previous character
     println("\n*** Exercise: Previous Character ***")
-    val userInput1 = readln().first()
-    val userInput2 = readln().first()
-    val userInput3 = readln().first()
-    val userInput4 = readln().first()
-    println(userInput1 - 1)
-    println(userInput2 - 1)
-    println(userInput3 - 1)
-    println(userInput4 - 1)
+    println(readln().first() - 1)
+    println(readln().first() - 1)
+    println(readln().first() - 1)
+    println(readln().first() - 1)
 
     println("\n*** End of Exercise ***")
 }
