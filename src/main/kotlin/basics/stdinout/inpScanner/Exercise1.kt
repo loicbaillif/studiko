fb46c37c8032a2ff309e819426637b12224851d1
loicbaillif <larmedeciel@gmail.com>
2023-04-03 07:33:59 +0200

    Kotlin/begin/inpScanner/ex1/v1 Scanner created

--- src/main/kotlin/basics/stdinout/inpScanner/Exercise1.kt
+++ src/main/kotlin/basics/stdinout/inpScanner/Exercise1.kt
@@ -1,9 +1,12 @@
 package basics.stdinout.inpScanner
 
+import java.util.Scanner
+
 fun exercise1() {
     // https://hyperskill.org/learn/step/4451
     // Printing each word in a new line
     println("*** Exercise: Printing each word in a new line ***")
+    var scanner = Scanner(System.`in`)
 
 
     println("\n*** End of Exercise ***")
