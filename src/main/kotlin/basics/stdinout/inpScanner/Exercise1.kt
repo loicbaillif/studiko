3379cab8a476a13903371ee15ad8a545daa38bf6
loicbaillif <larmedeciel@gmail.com>
2023-04-03 07:43:40 +0200

    Kotlin/begin/inpScanner/ex1/v3 Print outputs

--- src/main/kotlin/basics/stdinout/inpScanner/Exercise1.kt
+++ src/main/kotlin/basics/stdinout/inpScanner/Exercise1.kt
@@ -6,13 +6,17 @@ fun exercise1() {
     // https://hyperskill.org/learn/step/4451
     // Printing each word in a new line
     println("*** Exercise: Printing each word in a new line ***")
-    var scanner = Scanner(System.`in`)
+    val scanner = Scanner(System.`in`)
     val userInput1 = scanner.next()
     val userInput2 = scanner.next()
     val userInput3 = scanner.next()
     val userInput4 = scanner.next()
     val userInput5 = scanner.next()
-
+    println(userInput1)
+    println(userInput2)
+    println(userInput3)
+    println(userInput4)
+    println(userInput5)
 
     println("\n*** End of Exercise ***")
 }
