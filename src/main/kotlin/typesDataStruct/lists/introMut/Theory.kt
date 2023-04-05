ba237bd1432a3f0fbb52db301358b51c2cc4d844
loicbaillif <larmedeciel@gmail.com>
2023-04-05 21:39:46 +0200

    Kotlin/begin/introMutList/th/v1 First MutableList

--- src/main/kotlin/typesDataStruct/lists/introMut/Theory.kt
+++ src/main/kotlin/typesDataStruct/lists/introMut/Theory.kt
@@ -4,6 +4,11 @@ fun theory() {
     // https://hyperskill.org/learn/step/14879
     println("***** Theory *****")
 
+    println("\n1) Introduction to MutableList")
+    println("First declaration:")
+    val numbersList = mutableListOf(12.3, 45.6, 7.8, 90)
+    println(numbersList)
+
 
     println("\n\n***** End of theory *****")
 }
