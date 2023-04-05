e84e8f8b8bfd9a713a47db7f2c4d4e8e18cd7cd1
loicbaillif <larmedeciel@gmail.com>
2023-04-05 22:02:08 +0200

    Kotlin/begin/introMutList/th/v2 MutableList of Int

--- src/main/kotlin/typesDataStruct/lists/introMut/Theory.kt
+++ src/main/kotlin/typesDataStruct/lists/introMut/Theory.kt
@@ -9,6 +9,11 @@ fun theory() {
     val numbersList = mutableListOf(12.3, 45.6, 7.8, 90)
     println(numbersList)
 
+    println("\n2) Creating a MutableList with specified elements")
+    // Declaring a mutable list of integers
+    val mutableList1 = mutableListOf<Int>(1, 3, 5, 7, 9)
+    println(mutableList1)
+
 
     println("\n\n***** End of theory *****")
 }
