6ef497f49da268e91842ac8dda8744a6b1e22733
loicbaillif <larmedeciel@gmail.com>
2023-04-05 21:21:12 +0200

    Kotlin/begin/charac/ex3/init Comparing numbers and characters

---
+++ src/main/kotlin/typesDataStruct/basicOps/charac/Exercise3.kt
@@ -0,0 +1,10 @@
+package typesDataStruct.basicOps.charac
+
+fun exercise3() {
+    // https://hyperskill.org/learn/step/12309
+    // Exercise: Comparing numbers and characters
+    println("\n*** Exercise: Comparing numbers and characters ***")
+
+
+    println("\n*** End of exercise ***")
+}
