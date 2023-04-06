ab9fa648f7ba351fe6a171934208298c4d3e757d
loicbaillif <larmedeciel@gmail.com>
2023-04-06 06:45:10 +0200

    Kotlin/begin/conversion/ex2/init Sum of Longs

---
+++ src/main/kotlin/typesDataStruct/dataTypes/conver/Exercise3.kt
@@ -0,0 +1,11 @@
+package typesDataStruct.dataTypes.conver
+
+fun exercise3() {
+    // https://hyperskill.org/learn/step/16965
+    // Sum of Longs
+    println("\n*** Exercise: Sum of Longs ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/typesDataStruct/dataTypes/conver/Index.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/conver/Index.kt
@@ -4,5 +4,6 @@ fun index() {
     println("********** Type Conversion **********\n")
     // theory()
     // exercise1()
-    exercise2()
+    // exercise2()
+    exercise3()
 }
