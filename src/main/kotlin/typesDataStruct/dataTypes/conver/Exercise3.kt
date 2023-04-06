8ee4d8a920ba6ea1bbad86207d2550d5862266d4
loicbaillif <larmedeciel@gmail.com>
2023-04-06 06:45:54 +0200

    Kotlin/begin/conversion/ex2/v1 Variables created

--- src/main/kotlin/typesDataStruct/dataTypes/conver/Exercise3.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/conver/Exercise3.kt
@@ -4,7 +4,8 @@ fun exercise3() {
     // https://hyperskill.org/learn/step/16965
     // Sum of Longs
     println("\n*** Exercise: Sum of Longs ***")
-
+    val userLong1 = readln().toLong()
+    val userLong2 = readln().toLong()
 
 
     println("\n*** End of exercise ***")
