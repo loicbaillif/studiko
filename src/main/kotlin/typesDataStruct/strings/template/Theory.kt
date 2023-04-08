da3fb34d18f8c0d5b28aa31064559167d9cef42e
loicbaillif <larmedeciel@gmail.com>
2023-04-08 18:12:27 +0200

    Kotlin/begin/strTemp/th/v1 First example

--- src/main/kotlin/typesDataStruct/strings/template/Theory.kt
+++ src/main/kotlin/typesDataStruct/strings/template/Theory.kt
@@ -4,7 +4,9 @@ fun theory() {
     // https://hyperskill.org/learn/step/4547
     println("***** Theory *****")
 
-
+    var city = "Paris"
+    var temp = 13
+    println("The actual temperature in $city is $temp degrees Celcius.")
 
     println("\n\n***** End of theory *****")
 }
