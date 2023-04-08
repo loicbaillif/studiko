ec3db1406a4b39f3d3fff65656e776f1d7e3d20d
loicbaillif <larmedeciel@gmail.com>
2023-04-08 18:20:04 +0200

    Kotlin/begin/strTemp/th/v3 template in var

--- src/main/kotlin/typesDataStruct/strings/template/Theory.kt
+++ src/main/kotlin/typesDataStruct/strings/template/Theory.kt
@@ -10,6 +10,10 @@ fun theory() {
     city = "Roma"
     temp = 19
     println("The actual temperature in $city is $temp degrees Celcius.")
+    val value = 32
+    val currency = "Euros"
+    val price = "$value $currency"
+    println(price)
 
     println("\n\n***** End of theory *****")
 }
