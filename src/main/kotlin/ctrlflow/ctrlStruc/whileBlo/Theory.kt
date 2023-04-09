b9183c32b521046b1834d13aa5413d8118f11b8f
loicbaillif <larmedeciel@gmail.com>
2023-04-09 19:15:52 +0200

    Kotlin/begin/whileBlo/th/v1 First example

--- src/main/kotlin/ctrlflow/ctrlStruc/whileBlo/Theory.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/whileBlo/Theory.kt
@@ -4,7 +4,11 @@ fun theory() {
     // https://hyperskill.org/learn/step/4659
     println("***** Theory *****")
 
-
+    println("\n1) While loop")
+    var someInt = 0
+    while (someInt < 5) {
+        println("\t. i = ${someInt++}")
+    }
 
 
     println("\n***** End of theory *****")
