26539d96a9e8a78c137bedd4990f772173d8235a
loicbaillif <larmedeciel@gmail.com>
2023-04-09 15:46:06 +0200

    Kotlin/begin/ifEx/ex3/v1 get user inputs

--- src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise3.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise3.kt
@@ -5,6 +5,8 @@ fun exercise3() {
     // The max of two numbers without else-branch
     println("*** Exercise: The max of two numbers without else-branch ***")
 
+    val inputA = readln().toInt()
+    val inputB = readln().toInt()
 
 
     println("\n*** End of exercise ***")
