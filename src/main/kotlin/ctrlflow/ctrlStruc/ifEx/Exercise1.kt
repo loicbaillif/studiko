a4da621d7496eb123bbbb491d7e67322020b1167
loicbaillif <larmedeciel@gmail.com>
2023-04-09 11:58:36 +0200

    Kotlin/begin/ifEx/ex1/v1 Get user input

--- src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise1.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise1.kt
@@ -4,7 +4,7 @@ fun exercise1() {
     // https://hyperskill.org/learn/step/4598
     // Odd or even
     println("*** Exercise: Odd or even ***")
-
+    val userInput = readln().toInt()
 
 
     println("\n*** End of exercise ***")
