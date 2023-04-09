410f8228e7532fc8500e016199ca34d1c384f126
loicbaillif <larmedeciel@gmail.com>
2023-04-09 14:47:34 +0200

    Kotlin/begin/ifEx/ex2/init Exercise: Triangle

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise2.kt
@@ -0,0 +1,10 @@
+package ctrlflow.ctrlStruc.ifEx
+
+fun exercise2() {
+    // https://hyperskill.org/learn/step/4607
+    // Triangle
+    println("*** Exercise: Triangle ***")
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Index.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Index.kt
@@ -3,5 +3,6 @@ package ctrlflow.ctrlStruc.ifEx
 fun index() {
     println("********** If Expression **********\n")
     // theory()
-    exercise1()
+    // exercise1()
+    exercise2()
 }
