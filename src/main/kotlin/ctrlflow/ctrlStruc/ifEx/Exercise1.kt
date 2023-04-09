b529499c5f94f48b2f34fc4f56470b0d92a40ba9
loicbaillif <larmedeciel@gmail.com>
2023-04-09 12:06:05 +0200

    Kotlin/begin/ifEx/ex1/v3 Second proposal

--- src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise1.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise1.kt
@@ -5,11 +5,7 @@ fun exercise1() {
     // Odd or even
     println("*** Exercise: Odd or even ***")
     val userInput = readln().toInt()
-    if (userInput % 2 == 0) {
-        println("EVEN")
-    } else {
-        println("ODD")
-    }
+    println(if (userInput % 2 == 0) "EVEN" else "ODD")
 
 
     println("\n*** End of exercise ***")
