ee81be55a479f65518c0f377c4fcf6f7854525c0
loicbaillif <larmedeciel@gmail.com>
2023-04-09 17:25:54 +0200

    Kotlin/begin/repBlo/ex4/v1 Variables created

--- src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Exercise4.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Exercise4.kt
@@ -1,9 +1,16 @@
 package ctrlflow.ctrlStruc.repBlo
 
+const val LARGER = 1
+const val PERFECT = 0
+const val SMALLER = -1
+
 fun exercise4() {
     // https://hyperskill.org/learn/step/4645
     // Repeating Blocks
     println("*** Exercise: Repeating Blocks ***")
+
+    val nbInputs = readln().toInt()
+
 
     println("\n*** End of exercise ***")
 }
