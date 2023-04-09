fc0912a740bce3e8f5bf4846eab28eddc44a4e9a
loicbaillif <larmedeciel@gmail.com>
2023-04-09 16:10:15 +0200

    Kotlin/begin/repBlo/th/v1 First example

--- src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Theory.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Theory.kt
@@ -3,7 +3,10 @@ package ctrlflow.ctrlStruc.repBlo
 fun theory() {
     println("***** Theory *****")
 
-
+    println("\n1) Repeat Loop")
+    repeat(3) {
+        println("\t. Hello and Welcome!")
+    }
 
     println("\n***** End of theory *****")
 }
