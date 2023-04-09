c4694809c2533c099fe48ca8598977a6425c80d4
loicbaillif <larmedeciel@gmail.com>
2023-04-09 17:23:57 +0200

    Kotlin/begin/repBlo/ex4/init Repeating Blocks - init

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Exercise4.kt
@@ -0,0 +1,9 @@
+package ctrlflow.ctrlStruc.repBlo
+
+fun exercise4() {
+    // https://hyperskill.org/learn/step/4645
+    // Repeating Blocks
+    println("*** Exercise: Repeating Blocks ***")
+
+    println("\n*** End of exercise ***")
+}
