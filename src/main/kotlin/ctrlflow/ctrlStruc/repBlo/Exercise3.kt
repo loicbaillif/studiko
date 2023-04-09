af68547ab6a4b7fc4e00234bbdd47a842882716c
loicbaillif <larmedeciel@gmail.com>
2023-04-09 16:52:27 +0200

    Kotlin/begin/repBlo/ex3/init Maximum element divisible by 4

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Exercise3.kt
@@ -0,0 +1,11 @@
+package ctrlflow.ctrlStruc.repBlo
+
+fun exercise3() {
+    // https://hyperskill.org/learn/step/4648
+    // Exercise: Maximum element divisible by 4
+    println("*** Exercise: Maximum element divisible by 4 ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Index.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Index.kt
@@ -4,5 +4,6 @@ fun index() {
     println("********** Repeating Blocks **********\n")
     // theory()
     // exercise1()
-    exercise2()
+    // exercise2()
+    exercise3()
 }
