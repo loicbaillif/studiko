df97125e3b8b7673a4204b3e64e0fc58eafc232f
loicbaillif <larmedeciel@gmail.com>
2023-04-09 10:07:14 +0200

    Kotlin/begin/ifEx/th/v1 First example

--- src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Theory.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Theory.kt
@@ -4,7 +4,11 @@ fun theory() {
     // https://hyperskill.org/learn/step/4625
     println("***** Theory *****")
 
-
+    println("\n1) The single if-case")
+    val userAge = readln().toInt()
+    if (userAge > 100) {
+        println("Not a rookie")
+    }
 
     println("\n***** End of theory *****")
 }
