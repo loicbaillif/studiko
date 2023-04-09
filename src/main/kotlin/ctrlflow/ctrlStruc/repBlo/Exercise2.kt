7e3bcc13466710dd4ac3283e94f907ab2c3d3b84
loicbaillif <larmedeciel@gmail.com>
2023-04-09 16:42:21 +0200

    Kotlin/begin/repBlo/ex2/init Kotlin * 5

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Exercise2.kt
@@ -0,0 +1,9 @@
+package ctrlflow.ctrlStruc.repBlo
+
+fun exercise2() {
+    // https://hyperskill.org/learn/step/4431
+    // Kotlin * 5
+    println("*** Exercise: Kotlin * 5 ***")
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Index.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/repBlo/Index.kt
@@ -3,5 +3,6 @@ package ctrlflow.ctrlStruc.repBlo
 fun index() {
     println("********** Repeating Blocks **********\n")
     // theory()
-    exercise1()
+    // exercise1()
+    exercise2()
 }
