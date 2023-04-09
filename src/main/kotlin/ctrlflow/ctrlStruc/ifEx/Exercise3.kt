7675ea49d7eafac5b69024a1e02a8f229bc92d03
loicbaillif <larmedeciel@gmail.com>
2023-04-09 15:43:40 +0200

    Kotlin/begin/ifEx/ex3/init The max of 2 numbers (no else branch)

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise3.kt
@@ -0,0 +1,11 @@
+package ctrlflow.ctrlStruc.ifEx
+
+fun exercise3() {
+    // https://hyperskill.org/learn/step/4626
+    // The max of two numbers without else-branch
+    println("*** Exercise: The max of two numbers without else-branch ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Index.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Index.kt
@@ -4,5 +4,6 @@ fun index() {
     println("********** If Expression **********\n")
     // theory()
     // exercise1()
-    exercise2()
+    // exercise2()
+    exercise3()
 }
