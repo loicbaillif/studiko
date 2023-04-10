abd56edc09a9b0a10a384f958269897db90119b8
loicbaillif <larmedeciel@gmail.com>
2023-04-10 21:09:49 +0200

    Kotlin/begin/whileBlo/ex1/v5 DEBUG: create list of int from input

--- src/main/kotlin/ctrlflow/ctrlStruc/whileBlo/Exercise1.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/whileBlo/Exercise1.kt
@@ -1,28 +1,18 @@
 package ctrlflow.ctrlStruc.whileBlo
 
-import java.util.Scanner
-
 fun exercise1() {
     // https://hyperskill.org/learn/step/4720
     // Balance Checker
     println("*** Exercise: Balance Checker ***")
 
     var balance = readln().toInt()
+    var purchases = readln().split(' ').map { it.toInt()}.toMutableList()
     var enoughMoney = true
-    var purchase = 0
-    val scanner = Scanner(System.`in`)
 
-    while (balance >= 0 && scanner.hasNextInt()) {
-        purchase = readln().toInt()
-        balance -= purchase
-        if (balance < 0) enoughMoney = false
+    repeat(purchases.size) {
+        println(purchases[it])
     }
-
-    if (enoughMoney) {
-        println("Thank you for choosing us to manage your account! Your balance is $balance.")
-    } else {
-        println("Error, insufficient funds for the purchase. Your balance is $balance, but you need $purchase.")
-    }
+    println(purchases.size)
 
 
     println("\n*** End of exercise ***")
