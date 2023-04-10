6b3d5e655991b9fc4dadd6a36440849b60ae8458
loicbaillif <larmedeciel@gmail.com>
2023-04-10 20:47:37 +0200

    Kotlin/begin/whileBlo/th/v2 do...while

--- src/main/kotlin/ctrlflow/ctrlStruc/whileBlo/Theory.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/whileBlo/Theory.kt
@@ -17,11 +17,20 @@ fun theory() {
         println("\t. ${someChar++}")
     }
 
+    /*
     val scanner = Scanner(System.`in`)
     while (scanner.hasNext()) {
         val next = scanner.next()
         println(next)
     }
+     */
+
+    println("\n2) do...while loop")
+    do {
+        val userInput1 = readln().toInt()
+        println("You entered another positive number")
+    } while (userInput1 > 0)
+    println("Negative or 0; this is the end")
 
 
     println("\n***** End of theory *****")
