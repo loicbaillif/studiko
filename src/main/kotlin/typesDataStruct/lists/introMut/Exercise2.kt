efa2a20532a2a151bb76e22837a7254309079d67
loicbaillif <larmedeciel@gmail.com>
2023-04-12 09:06:27 +0200

    Kotlin/begin/introMutList/ex2/init Initializing a list

---
+++ src/main/kotlin/typesDataStruct/lists/introMut/Exercise2.kt
@@ -0,0 +1,11 @@
+package typesDataStruct.lists.introMut
+
+fun exercise2() {
+    // https://hyperskill.org/learn/daily/14882
+    // Initializing a list
+    println("*** Exercise: Initializing a list ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/typesDataStruct/lists/introMut/Index.kt
+++ src/main/kotlin/typesDataStruct/lists/introMut/Index.kt
@@ -3,5 +3,6 @@ package typesDataStruct.lists.introMut
 fun index() {
     println("********** Introduction to MutableList **********\n")
     // theory()
-    exercise1()
+    // exercise1()
+    exercise2()
 }
