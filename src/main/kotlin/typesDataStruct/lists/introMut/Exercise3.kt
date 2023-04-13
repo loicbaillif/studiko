211497c649f0594a9773320d123ee08e3a20f291
loicbaillif <larmedeciel@gmail.com>
2023-04-13 07:06:23 +0200

    Kotlin/begin/introMutList/ex3/init Initializing a mut list of longs

---
+++ src/main/kotlin/typesDataStruct/lists/introMut/Exercise3.kt
@@ -0,0 +1,11 @@
+package typesDataStruct.lists.introMut
+
+fun exercise3() {
+    // https://hyperskill.org/learn/daily/14884
+    // Initializing a mutable list of longs
+    println("*** Exercise: Initializing a mutable list of longs ***")
+
+
+
+    println("\n*** End of Exercise ***")
+}
--- src/main/kotlin/typesDataStruct/lists/introMut/Index.kt
+++ src/main/kotlin/typesDataStruct/lists/introMut/Index.kt
@@ -4,5 +4,6 @@ fun index() {
     println("********** Introduction to MutableList **********\n")
     // theory()
     // exercise1()
-    exercise2()
+    // exercise2()
+    exercise3()
 }
