344813ed300bed1fd75bb718bdffbc4326df7294
loicbaillif <larmedeciel@gmail.com>
2023-04-13 07:07:31 +0200

    Kotlin/begin/introMutList/ex3/v1 Define variable

--- src/main/kotlin/typesDataStruct/lists/introMut/Exercise3.kt
+++ src/main/kotlin/typesDataStruct/lists/introMut/Exercise3.kt
@@ -5,7 +5,7 @@ fun exercise3() {
     // Initializing a mutable list of longs
     println("*** Exercise: Initializing a mutable list of longs ***")
 
-
+    val longs = mutableListOf(100_000_000_001L, 100_000_000_002L, 100_000_000_003L)
 
     println("\n*** End of Exercise ***")
 }
