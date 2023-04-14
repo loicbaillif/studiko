54e6aeaa647afed5255e8c73837da994305ac0a5
loicbaillif <larmedeciel@gmail.com>
2023-04-14 22:02:48 +0200

    Kotlin/begin/equal/th/v1 First example

--- src/main/kotlin/typesDataStruct/variab/equality/Theory.kt
+++ src/main/kotlin/typesDataStruct/variab/equality/Theory.kt
@@ -4,6 +4,10 @@ fun theory() {
     // https://hyperskill.org/learn/step/11236
     println("***** Theory *****")
 
+    println("\n1) Comparison")
+    val someString1 = "Hello"
+    val someString2 = "Hello"
+    println("\"Hello\" == \"Hello\" ==> ${someString1 == someString2}")
 
 
     println("\n***** End of theory *****")
