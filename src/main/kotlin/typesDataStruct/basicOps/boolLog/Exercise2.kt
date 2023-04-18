acb67302f4c2dc34e38d2cb9b9d42fa68278a25c
loicbaillif <larmedeciel@gmail.com>
2023-04-18 19:20:18 +0200

    Kotlin/begin/boolLog/ex2/init Invitations and gifts

---
+++ src/main/kotlin/typesDataStruct/basicOps/boolLog/Exercise2.kt
@@ -0,0 +1,11 @@
+package typesDataStruct.basicOps.boolLog
+
+fun exercise2() {
+    // https://hyperskill.org/learn/step/12310
+    // Invitations and gifts
+    println("*** Exercise: Invitations and gifts ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/typesDataStruct/basicOps/boolLog/Index.kt
+++ src/main/kotlin/typesDataStruct/basicOps/boolLog/Index.kt
@@ -3,5 +3,6 @@ package typesDataStruct.basicOps.boolLog
 fun index() {
     println("********** Boolean and Logical Operations **********\n")
     // theory()
-    exercise1()
+    // exercise1()
+    exercise2()
 }
