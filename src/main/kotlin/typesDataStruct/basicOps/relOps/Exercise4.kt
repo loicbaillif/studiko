328e1c1449390550cb75bdc3f317cb33e8e76a54
loicbaillif <larmedeciel@gmail.com>
2023-04-20 06:45:54 +0200

    Kotlin/begin/relOps/ex4/init Check the sum

---
+++ src/main/kotlin/typesDataStruct/basicOps/relOps/Exercise4.kt
@@ -0,0 +1,11 @@
+package typesDataStruct.basicOps.relOps
+
+fun exercise4() {
+    // https://hyperskill.org/learn/daily/4533
+    // Check the sum
+    println("*** Exercise: Check the sum ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
