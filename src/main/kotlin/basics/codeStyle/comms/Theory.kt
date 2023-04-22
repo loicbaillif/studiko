850ff61642b7f945b399ff2713eda229e8688e5a
loicbaillif <larmedeciel@gmail.com>
2023-04-22 11:46:27 +0200

    Kotlin/begin/comments/th/v2 Multi-line comment

--- src/main/kotlin/basics/codeStyle/comms/Theory.kt
+++ src/main/kotlin/basics/codeStyle/comms/Theory.kt
@@ -5,7 +5,10 @@ fun theory() {
     println("***** Theory *****")
 
     println("\n1) End of line comment")
-    println("Anything placed after \\\\, until the end of the line")
+    println("Anything placed after //, until the end of the line")
+
+    println("\n2) Multi-line comment")
+    println("Anything placed between /* and */, even if separated by lines")
 
     println("\n***** End of theory *****")
 }
