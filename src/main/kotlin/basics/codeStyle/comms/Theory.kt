10a57dceab04bbfb68dea8381b839a180d3ccd5f
loicbaillif <larmedeciel@gmail.com>
2023-04-22 11:42:19 +0200

    Kotlin/begin/comments/th/v1 End of line comment

--- src/main/kotlin/basics/codeStyle/comms/Theory.kt
+++ src/main/kotlin/basics/codeStyle/comms/Theory.kt
@@ -4,7 +4,8 @@ fun theory() {
     // https://hyperskill.org/learn/step/4406
     println("***** Theory *****")
 
-
+    println("\n1) End of line comment")
+    println("Anything placed after \\\\, until the end of the line")
 
     println("\n***** End of theory *****")
 }
