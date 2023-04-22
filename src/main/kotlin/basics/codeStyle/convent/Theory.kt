08adb134db2b337bb1c8269fe221c88311ef23b5
loicbaillif <larmedeciel@gmail.com>
2023-04-22 15:10:31 +0200

    Kotlin/begin/convent/th/v2 First rules

--- src/main/kotlin/basics/codeStyle/convent/Theory.kt
+++ src/main/kotlin/basics/codeStyle/convent/Theory.kt
@@ -12,5 +12,11 @@ fun theory() {
     println("Online resource")
     // https://kotlinlang.org/docs/coding-conventions.html#configure-style-in-ide
 
+    println("\n2) First steps")
+    println("\t. Use 4 spaces for indentation")
+    println("\t. Omit semicolon, not needed for Kotlin")
+    println("\t. Place opening curly brace at the end of the line")
+    println("\t. Place closing curly brace at the beginning of the next line")
+
     println("\n***** End of theory *****")
 }
