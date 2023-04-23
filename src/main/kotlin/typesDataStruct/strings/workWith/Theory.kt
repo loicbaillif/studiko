f0633ac7d596be6cac99f556e2bbb3d6cb75ef41
loicbaillif <larmedeciel@gmail.com>
2023-04-23 17:37:15 +0200

    Kotlin/begin/workStr/th/v4 first(), last(), lastIndex

--- src/main/kotlin/typesDataStruct/strings/workWith/Theory.kt
+++ src/main/kotlin/typesDataStruct/strings/workWith/Theory.kt
@@ -11,13 +11,16 @@ fun theory() {
 
     println("\n1) Accessing characters")
     val someString1 = "Welcome to Kotlin"
-    println("string1 = $someString1")
-    println("string1[0] = ${someString1[0]}")
-    println("string1[1] = ${someString1[1]}")
-    println("string1[3] = ${someString1[3]}")
-    println("string1[10] = ${someString1[10]}")
-    println("string1.length = ${someString1.length}")
-    println("string1[string1.length - 1] = ${someString1[someString1.length - 1]}")
+    println("\t. string1 = $someString1")
+    println("\t. string1[0] = ${someString1[0]}")
+    println("\t. string1[1] = ${someString1[1]}")
+    println("\t. string1[3] = ${someString1[3]}")
+    println("\t. string1[10] = ${someString1[10]}")
+    println("\t. string1.length = ${someString1.length}")
+    println("\t. string1[string1.length - 1] = ${someString1[someString1.length - 1]}")
+    println("\t. string1.first() = ${someString1.first()}")
+    println("\t. string1.last() = ${someString1.last()}")
+    println("\t. string1.lastIndex = ${someString1.lastIndex}")
 
 
     println("\n***** End of theory *****")
