0e39b7a86d2fe00096e119f41a01de87b8a2035b
loicbaillif <larmedeciel@gmail.com>
2023-04-23 18:33:24 +0200

    Kotlin/begin/chattybot/st3/init Stage 3 / 5

--- src/main/kotlin/projects/chattybot/Index.kt
+++ src/main/kotlin/projects/chattybot/Index.kt
@@ -5,7 +5,8 @@ fun index() {
     println("********** PROJECT : CHATTY BOT **********\n")
 
     // stage1()
-    stage2()
+    // stage2()
+    stage3()
 
     println("\n\n********** END OF PROJECT **********")
 }
---
+++ src/main/kotlin/projects/chattybot/Stage3.kt
@@ -0,0 +1,11 @@
+package projects.chattybot
+
+/**
+ * https://hyperskill.org/projects/126/stages/670/implement
+ * Stage 3/5 : Guess the age
+ * @author JetBrains Academy
+ */
+
+fun stage3() {
+
+}
