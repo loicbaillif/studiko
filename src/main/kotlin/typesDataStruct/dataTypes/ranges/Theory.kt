0de65cdde38987c536f2dc619fbdeeb161ca6ec9
loicbaillif <larmedeciel@gmail.com>
2023-04-25 16:03:40 +0200

    Kotlin/begin/ranges/th/v3 Value not in interval

--- src/main/kotlin/typesDataStruct/dataTypes/ranges/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/ranges/Theory.kt
@@ -8,12 +8,20 @@ package typesDataStruct.dataTypes.ranges
 fun theory() {
     println("***** Theory *****")
 
+    println("\n1) Value in interval")
+
     val lowLimit = 3
     val highLimit = 10
     println("\t. 3 in 3..10 ==> ${3 in lowLimit..highLimit}")
-    println("\t. 8 in 3..10 ==> ${3 in lowLimit..highLimit}")
-    println("\t. 10 in 3..10 ==> ${3 in lowLimit..highLimit}")
-    println("\t. 11 in 3..10 ==> ${3 in lowLimit..highLimit}")
+    println("\t. 8 in 3..10 ==> ${8 in lowLimit..highLimit}")
+    println("\t. 10 in 3..10 ==> ${10 in lowLimit..highLimit}")
+    println("\t. 11 in 3..10 ==> ${11 in lowLimit..highLimit}")
+
+    println("\n2) Value not in interval")
+    println("\t. 3 !in 3..10 ==> ${3 !in lowLimit..highLimit}")
+    println("\t. 8 !in 3..10 ==> ${8 !in lowLimit..highLimit}")
+    println("\t. 10 !in 3..10 ==> ${10 !in lowLimit..highLimit}")
+    println("\t. 11 !in 3..10 ==> ${11 !in lowLimit..highLimit}")
 
     println("\n***** End of Theory *****")
 }
