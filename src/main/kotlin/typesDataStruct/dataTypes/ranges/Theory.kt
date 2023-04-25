5e9a9ced5b9becfac5396aaf19f567280ff1a569
loicbaillif <larmedeciel@gmail.com>
2023-04-25 15:58:40 +0200

    Kotlin/begin/ranges/th/v1 First example

--- src/main/kotlin/typesDataStruct/dataTypes/ranges/Theory.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/ranges/Theory.kt
@@ -8,6 +8,12 @@ package typesDataStruct.dataTypes.ranges
 fun theory() {
     println("***** Theory *****")
 
+    val lowLimit = 3
+    val highLimit = 10
+    println("Please enter a number:")
+    val userInput = readln().toInt()
+    print("$userInput in $lowLimit..$highLimit ==> ")
+    println(userInput in lowLimit..highLimit)
 
 
     println("\n***** End of Theory *****")
