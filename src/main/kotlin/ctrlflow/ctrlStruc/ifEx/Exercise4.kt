2e3577d0fec578b1196f66b46f20556144155b32
loicbaillif <larmedeciel@gmail.com>
2023-04-26 07:08:46 +0200

    Kotlin/begin/ifEx/ex4/init The max of three numbers

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise4.kt
@@ -0,0 +1,15 @@
+package ctrlflow.ctrlStruc.ifEx
+
+/**
+ * Exercise: The max of three numbers
+ * https://hyperskill.org/learn/daily/4627
+ * @author JetBrains Academy
+ */
+
+fun exercise4() {
+    println("*** Exercise: The max of three numbers ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
