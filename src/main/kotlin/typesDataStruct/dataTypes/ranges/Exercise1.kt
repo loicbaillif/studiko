3f5908481d1183c3e346b48d5db4340d01f07808
loicbaillif <larmedeciel@gmail.com>
2023-04-26 20:30:52 +0200

    Kotlin/begin/ranges/ex1/v1 Limits defined as constants

--- src/main/kotlin/typesDataStruct/dataTypes/ranges/Exercise1.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/ranges/Exercise1.kt
@@ -6,6 +6,9 @@ package typesDataStruct.dataTypes.ranges
  * @author JetBrains Academy
  */
 
+const val LOW_LIMIT = 1
+const val HIGH_LIMIT = 10
+
 fun exercise1() {
     println("*** Exercise: Not in range ***")
 
