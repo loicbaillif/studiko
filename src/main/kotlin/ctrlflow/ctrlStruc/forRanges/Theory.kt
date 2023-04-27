a4eab260752f895cc72305b3d6bc4ba9d15ad32a
loicbaillif <larmedeciel@gmail.com>
2023-04-27 21:34:56 +0200

    Kotlin/begin/forRanges/th/v3 Iterating through string

--- src/main/kotlin/ctrlflow/ctrlStruc/forRanges/Theory.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/forRanges/Theory.kt
@@ -8,13 +8,18 @@ package ctrlflow.ctrlStruc.forRanges
 fun theory() {
     println("***** Theory *****")
 
-    println("\n1) For loop")
+    println("\n*** 1) For loop")
+    println("Example 1:")
     for (i in 1..4) {
         println("\t. $i")
     }
-    println()
 
+    println("\nExample 2:")
     for (ch in 'a'..'d') println("\t. $ch")
+
+    println("\n*** 2) Iterating through a String")
+    val someString = "Serendipity"
+    for (ch in someString) println("\t. $ch")
 
 
     println("\n***** End of Theory *****")
