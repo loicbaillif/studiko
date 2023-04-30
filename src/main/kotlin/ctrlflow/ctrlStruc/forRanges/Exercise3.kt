b4959076774637a2160535f53a5b30c6a549b855
loicbaillif <larmedeciel@gmail.com>
2023-04-30 15:57:04 +0200

    Kotlin/begin/forRanges/ex3/init The longest sequence

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/forRanges/Exercise3.kt
@@ -0,0 +1,16 @@
+package ctrlflow.ctrlStruc.forRanges
+
+/**
+ * Exercise: The longest sequence
+ * https://hyperskill.org/learn/step/4702
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: The longest sequence ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/ctrlflow/ctrlStruc/forRanges/Index.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/forRanges/Index.kt
@@ -8,5 +8,6 @@ fun index() {
     println("********** For loop and ranges **********\n")
     // theory()
     // exercise1()
-    exercise2()
+    // exercise2()
+    exercise3()
 }
