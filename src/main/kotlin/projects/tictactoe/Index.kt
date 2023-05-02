1bcd943bf123135a2e9bf24753322b514e74fb03
loicbaillif <larmedeciel@gmail.com>
2023-05-02 07:24:07 +0200

    Kotlin/begin/tictactoe/st1/init Stage 1 : Welcome to the battlefield

--- src/main/kotlin/projects/tictactoe/Index.kt
+++ src/main/kotlin/projects/tictactoe/Index.kt
@@ -2,5 +2,7 @@ package projects.tictactoe
 fun index() {
     println("********** PROJECT : TIC TAC TOE **********\n")
 
+    stage1()
+
     println("\n\n********** END OF PROJECT **********")
 }
---
+++ src/main/kotlin/projects/tictactoe/Stage1.kt
@@ -0,0 +1,12 @@
+package projects.tictactoe
+
+/**
+ * Stage 1 / 5: Welcome to the battlefield
+ * https://hyperskill.org/projects/123/stages/654/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage1() {
+
+}
