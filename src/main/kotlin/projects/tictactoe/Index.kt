7fd20178459c79e008ad7f989c615cee98358d79
loicbaillif <larmedeciel@gmail.com>
2023-05-03 21:02:45 +0200

    Kotlin/begin/tictactoe/st2/init The user is the gamemaster

--- src/main/kotlin/projects/tictactoe/Index.kt
+++ src/main/kotlin/projects/tictactoe/Index.kt
@@ -3,6 +3,7 @@ fun index() {
     println("********** PROJECT : TIC TAC TOE **********\n")
 
     stage1()
+    stage2()
 
     println("\n\n********** END OF PROJECT **********")
 }
---
+++ src/main/kotlin/projects/tictactoe/Stage2.kt
@@ -0,0 +1,12 @@
+package projects.tictactoe
+
+/**
+ * Stage 2 / 5 : The user is the gamemaster
+ * https://hyperskill.org/projects/123/stages/655/implement
+ * @author : JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage2() {
+
+}
