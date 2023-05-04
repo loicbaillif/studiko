f8af135aa379d0afa4e3151a5db3ba75af279d3e
loicbaillif <larmedeciel@gmail.com>
2023-05-04 19:18:18 +0200

    Kotlin/begin/workMut/th/v1 First example

--- src/main/kotlin/typesDataStruct/lists/workMut/Theory.kt
+++ src/main/kotlin/typesDataStruct/lists/workMut/Theory.kt
@@ -9,6 +9,12 @@ package typesDataStruct.lists.workMut
 fun theory() {
     println("***** Theory *****")
 
+    println("1) Outputting a list")
+    val southernCross = mutableListOf("Acrux", "Gacrux", "Imai", "Mimosa")
+    println("\t. starsList = $southernCross")
+    println("\t. starsList.joinToString() = ${southernCross.joinToString()}")
+    println("\t. starsList.joinToString(\" then \") = ${southernCross.joinToString("then")}")
+
 
 
     println("\n***** End of theory *****")
