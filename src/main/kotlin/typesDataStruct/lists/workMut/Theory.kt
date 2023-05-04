d7a9815b5c5017be01841e974685eff639544759
loicbaillif <larmedeciel@gmail.com>
2023-05-04 19:36:09 +0200

    Kotlin/begin/workMut/th/v2 Summing lists

--- src/main/kotlin/typesDataStruct/lists/workMut/Theory.kt
+++ src/main/kotlin/typesDataStruct/lists/workMut/Theory.kt
@@ -9,12 +9,18 @@ package typesDataStruct.lists.workMut
 fun theory() {
     println("***** Theory *****")
 
-    println("1) Outputting a list")
+    println("\n1) Outputting a list")
     val southernCross = mutableListOf("Acrux", "Gacrux", "Imai", "Mimosa")
     println("\t. starsList = $southernCross")
     println("\t. starsList.joinToString() = ${southernCross.joinToString()}")
     println("\t. starsList.joinToString(\" then \") = ${southernCross.joinToString("then")}")
 
+    println("\n2) Working with multiple lists")
+    val starsList1 = mutableListOf("Ginan", "Mu Crucis")
+    val tempList1 = southernCross + starsList1
+    println("\t. starsList = $southernCross")
+    println("\t. starsList2 = $starsList1")
+    println("starsList + starsList2 = $tempList1")
 
 
     println("\n***** End of theory *****")
