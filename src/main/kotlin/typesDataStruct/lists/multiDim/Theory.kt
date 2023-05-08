c0290363e46aff89138b04534b92a8ae9a56016d
loicbaillif <larmedeciel@gmail.com>
2023-05-08 18:10:10 +0200

    Kotlin/begin/multiDim/th/v3 Accessing elements - part 1

--- src/main/kotlin/typesDataStruct/lists/multiDim/Theory.kt
+++ src/main/kotlin/typesDataStruct/lists/multiDim/Theory.kt
@@ -23,5 +23,12 @@ fun theory() {
     )
     println(mutList2D_2)
 
+    println("\n*** 2) Accessing elements")
+    val mutList2D_3 = mutableListOf<MutableList<Int>>(
+        mutableListOf(0, 1, 2),
+        mutableListOf(3, 4, 5),
+        mutableListOf(6, 7, 8)
+    )
+
     println("\n***** End of theory *****")
 }
