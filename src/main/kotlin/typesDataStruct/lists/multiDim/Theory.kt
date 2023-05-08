69ac647cd67186b5029cc568e04f3eca5ff49428
loicbaillif <larmedeciel@gmail.com>
2023-05-08 18:04:55 +0200

    Kotlin/begin/multiDim/th/v1 First example

--- src/main/kotlin/typesDataStruct/lists/multiDim/Theory.kt
+++ src/main/kotlin/typesDataStruct/lists/multiDim/Theory.kt
@@ -9,7 +9,13 @@ package typesDataStruct.lists.multiDim
 fun theory() {
     println("***** Theory *****")
 
-
+    println("\n*** 1) Creating 2-dimensional lists")
+    val mutList2D = mutableListOf(
+        mutableListOf(0, 0, 0, 0),
+        mutableListOf(0, 0, 0, 0),
+        mutableListOf(0, 0, 0, 0)
+    )
+    println(mutList2D)
 
     println("\n***** End of theory *****")
 }
