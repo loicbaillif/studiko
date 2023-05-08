b424d7f8de4241dd3034ee69719968d1fe296620
loicbaillif <larmedeciel@gmail.com>
2023-05-08 10:43:55 +0200

    Kotlin/begin/forLoop/ex1/v3 Fill lists

--- src/main/kotlin/typesDataStruct/lists/forLoop/Exercise1.kt
+++ src/main/kotlin/typesDataStruct/lists/forLoop/Exercise1.kt
@@ -10,9 +10,18 @@ package typesDataStruct.lists.forLoop
 fun exercise1() {
     println("*** Exercise: Individual Taxes ***")
 
+    // Init variables
     val nbCompanies = readln().toInt()
     val listIncomes = mutableListOf<Int>()
     val listTaxRates = mutableListOf<Int>()
+
+    // Fill lists
+    for (index in 1..nbCompanies) {
+        listIncomes.add(readln().toInt())
+    }
+    for (index in 1..nbCompanies) {
+        listTaxRates.add(readln().toInt())
+    }
 
     println("\n*** End of exercise ***")
 }
