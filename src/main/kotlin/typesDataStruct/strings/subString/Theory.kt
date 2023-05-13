7c6ed5f8026dd39ad0c1eabc13f1eeed7eb6dcf2
loicbaillif <larmedeciel@gmail.com>
2023-05-13 08:23:28 +0200

    Kotlin/begin/substring/th/v1 First examples

--- src/main/kotlin/typesDataStruct/strings/subString/Theory.kt
+++ src/main/kotlin/typesDataStruct/strings/subString/Theory.kt
@@ -9,7 +9,13 @@ package typesDataStruct.strings.subString
 fun theory() {
     println("***** Theory *****")
 
-
+    println("\n*** 1) Getting a part of a string")
+    val someString1 = "Hello"
+    println("\t. someString1.substring(0, 1) ==> ${someString1.substring(0, 1)}")
+    println("\t. someString1.substring(0, 3) ==> ${someString1.substring(0, 3)}")
+    println("\t. someString1.substring(1, 3) ==> ${someString1.substring(1, 3)}")
+    println("\t. someString1.substring(1) ==> ${someString1.substring(1)}")
+    println("\t. someString1.substring(3, 5) ==> ${someString1.substring(3, 5)}")
 
     println("\n***** End of theory *****")
 }
