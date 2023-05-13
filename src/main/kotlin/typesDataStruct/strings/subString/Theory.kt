53dd887501faeba80c0382a9891ad4603af2cf96
loicbaillif <larmedeciel@gmail.com>
2023-05-13 21:18:51 +0200

    Kotlin/begin/substring/th/v2 substringBefore, substringAfter

--- src/main/kotlin/typesDataStruct/strings/subString/Theory.kt
+++ src/main/kotlin/typesDataStruct/strings/subString/Theory.kt
@@ -11,11 +11,19 @@ fun theory() {
 
     println("\n*** 1) Getting a part of a string")
     val someString1 = "Hello"
-    println("\t. someString1.substring(0, 1) ==> ${someString1.substring(0, 1)}")
-    println("\t. someString1.substring(0, 3) ==> ${someString1.substring(0, 3)}")
-    println("\t. someString1.substring(1, 3) ==> ${someString1.substring(1, 3)}")
-    println("\t. someString1.substring(1) ==> ${someString1.substring(1)}")
-    println("\t. someString1.substring(3, 5) ==> ${someString1.substring(3, 5)}")
+    println("\t. String1.substring(0, 1) ==> ${someString1.substring(0, 1)}")
+    println("\t. String1.substring(0, 3) ==> ${someString1.substring(0, 3)}")
+    println("\t. String1.substring(1, 3) ==> ${someString1.substring(1, 3)}")
+    println("\t. String1.substring(1) ==> ${someString1.substring(1)}")
+    println("\t. String1.substring(3, 5) ==> ${someString1.substring(3, 5)}")
+
+    println("\n\t. String1.substringAfter('l') ==> ${someString1.substringAfter('l')}")
+    println("\t. String1.substringAfter('z') ==> ${someString1.substringAfter('z')}")
+    println("\t. String1.substringBefore('o') ==> ${someString1.substringAfter('o')}")
+    println("\t. String1.substringBefore('z') ==> ${someString1.substringAfter('z')}")
+
+
+
 
     println("\n***** End of theory *****")
 }
