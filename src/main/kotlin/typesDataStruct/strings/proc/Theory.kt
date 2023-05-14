cc20dacf9fa4de44b89be4a4da8bc2a373af42ec
loicbaillif <larmedeciel@gmail.com>
2023-05-14 12:47:48 +0200

    Kotlin/begin/procString/th/v2 Second example

--- src/main/kotlin/typesDataStruct/strings/proc/Theory.kt
+++ src/main/kotlin/typesDataStruct/strings/proc/Theory.kt
@@ -13,6 +13,10 @@ fun theory() {
     val someString1 = "I want to break free"
     val someList1 : List<String> = someString1.split(" ")
     println("\t. $someList1")
+    val someString2 = "+1-555-345-6789"
+    val someList2 = someString2.split("-")
+    println("\t. $someList2")
+
 
     println("\n***** End of theory ***")
 }
