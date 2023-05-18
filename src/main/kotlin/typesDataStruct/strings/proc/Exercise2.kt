ebe60e351d66ea478be980ff11655393acb2a29b
loicbaillif <larmedeciel@gmail.com>
2023-05-18 09:42:06 +0200

    Kotlin/begin/procString/ex2/v2 Loop to treat inputs

--- src/main/kotlin/typesDataStruct/strings/proc/Exercise2.kt
+++ src/main/kotlin/typesDataStruct/strings/proc/Exercise2.kt
@@ -15,6 +15,13 @@ fun exercise2() {
     val userInput = readln()
     var result = 0 // Nb of letters to have to get an euphonious
     var consecutiveCount = 0 // nb of consecutive vow/cons
+    var previousIsVowel = userInput.first() in VOWELS
+
+    for (ch in userInput) {
+        if (previousIsVowel == ch in VOWELS) {
+            // The streak is on
+        }
+    }
 
     println("\n*** End of exercise ***")
 }
