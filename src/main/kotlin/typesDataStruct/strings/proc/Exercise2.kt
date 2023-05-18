4343943ef10b34229b0f9b91f901bc46862c5a18
loicbaillif <larmedeciel@gmail.com>
2023-05-18 09:28:50 +0200

    Kotlin/begin/procString/ex2/v1 Variables

--- src/main/kotlin/typesDataStruct/strings/proc/Exercise2.kt
+++ src/main/kotlin/typesDataStruct/strings/proc/Exercise2.kt
@@ -7,10 +7,14 @@ package typesDataStruct.strings.proc
  * Solution by Loïc Baillif
  */
 
+const val VOWELS = "aeiouy"
+
 fun exercise2() {
     println("*** Exercise: Create an euphonious word ***")
 
-
+    val userInput = readln()
+    var result = 0 // Nb of letters to have to get an euphonious
+    var consecutiveCount = 0 // nb of consecutive vow/cons
 
     println("\n*** End of exercise ***")
 }
