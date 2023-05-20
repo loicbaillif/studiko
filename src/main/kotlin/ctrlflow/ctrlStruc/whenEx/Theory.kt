4a487fbd4e603d5889a2f66abf6fcabbd6800fe6
loicbaillif <larmedeciel@gmail.com>
2023-05-20 17:04:39 +0200

    Kotlin/begin/when/th/v1 First example

--- src/main/kotlin/ctrlflow/ctrlStruc/whenEx/Theory.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/whenEx/Theory.kt
@@ -3,6 +3,18 @@ package ctrlflow.ctrlStruc.whenEx
 fun theory() {
     println("***** Theory *****")
 
+    println("\n*** 1) Alternatives")
+    val (var1, op, var2) = readln().split(" ")
+
+    val a = var1.toInt()
+    val b = var2.toInt()
+
+    when (op) {
+        "+" -> println(a + b)
+        "-" -> println(a - b)
+        "*" -> println(a * b)
+        else -> println("Unknown operator")
+    }
 
 
     println("\n***** End of theory *****")
