34db29f8e8e7b22d7296aa4f8d7b4b4cc4af9982
loicbaillif <larmedeciel@gmail.com>
2023-05-21 13:16:26 +0200

    Kotlin/begin/when/ex3/init The army of units

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/whenEx/Exercise3.kt
@@ -0,0 +1,16 @@
+package ctrlflow.ctrlStruc.whenEx
+
+/**
+ * Exercise: The army of units
+ * https://hyperskill.org/learn/step/4610
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: The army of units ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
