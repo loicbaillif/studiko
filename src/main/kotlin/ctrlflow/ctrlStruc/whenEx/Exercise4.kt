e46d011395fed1ca56564cd629236d6592eb650a
loicbaillif <larmedeciel@gmail.com>
2023-05-21 13:38:23 +0200

    Kotlin/begin/when/ex4/init Just checking

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/whenEx/Exercise4.kt
@@ -0,0 +1,16 @@
+package ctrlflow.ctrlStruc.whenEx
+
+/**
+ * Exercise: Just checking
+ * https://hyperskill.org/learn/step/4602
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise4() {
+    println("*** Exercise: Just checking ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
