da0c726567075bbc66a8679cb59413ee53e0f798
loicbaillif <larmedeciel@gmail.com>
2023-05-27 20:49:41 +0200

    Kotlin/begin/cinema/st2/init Stage2 : Sold!

---
+++ src/main/kotlin/projects/cinema/Stage2.kt
@@ -0,0 +1,12 @@
+package projects.cinema
+
+/**
+ * Stage 2/5: Sold!
+ * https://hyperskill.org/projects/138/stages/736/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage2() {
+
+}
--- src/main/kotlin/projects/cinema/index.kt
+++ src/main/kotlin/projects/cinema/index.kt
@@ -7,7 +7,8 @@ package projects.cinema
 fun index() {
     println("********** PROJECT : CINEMA ROOM MANAGER **********\n")
 
-    stage1()
+    // stage1()
+    stage2()
 
     println("\n\n********** END OF PROJECT **********")
 }
