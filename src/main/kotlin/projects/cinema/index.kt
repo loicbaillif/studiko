883779a89c02262fd97d978f3e972c9942673d5b
loicbaillif <larmedeciel@gmail.com>
2023-05-27 20:33:38 +0200

    Kotlin/begin/cinema/st1/init Stqge 1: init

--- src/main/kotlin/projects/cinema/index.kt
+++ src/main/kotlin/projects/cinema/index.kt
@@ -7,6 +7,7 @@ package projects.cinema
 fun index() {
     println("********** PROJECT : CINEMA ROOM MANAGER **********\n")
 
+    stage1()
 
     println("\n\n********** END OF PROJECT **********")
 }
---
+++ src/main/kotlin/projects/cinema/stage1.kt
@@ -0,0 +1,12 @@
+package projects.cinema
+
+/**
+ * Stage 1/5: Arrangement
+ * https://hyperskill.org/projects/138/stages/735/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage1() {
+
+}
