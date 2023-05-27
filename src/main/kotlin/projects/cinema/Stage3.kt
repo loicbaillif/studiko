997fa722675e345064350455dd0d607ba9b22b45
loicbaillif <larmedeciel@gmail.com>
2023-05-27 22:26:27 +0200

    Kotlin/begin/cinema/st3/init Stage 3: Tickets

---
+++ src/main/kotlin/projects/cinema/Stage3.kt
@@ -0,0 +1,12 @@
+package projects.cinema
+
+/**
+ * Stage3/5: Tickets
+ * https://hyperskill.org/projects/138/stages/737/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage3() {
+
+}
