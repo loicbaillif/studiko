73844cfe85f91241a5e9d9475d00b2916c045a1b
loicbaillif <larmedeciel@gmail.com>
2023-05-27 23:40:40 +0200

    Kotlin/begin/cinema/st3/v6 Define constants for user ticket

--- src/main/kotlin/projects/cinema/constants.kt
+++ src/main/kotlin/projects/cinema/constants.kt
@@ -2,6 +2,8 @@ package projects.cinema
 
 const val ASK_NB_ROWS = "Enter the number of rows:"
 const val ASK_NB_SEATS = "Enter the number of seats in each row:"
+const val ASK_TICKET_ROW = "Enter a row number:"
+const val ASK_TICKET_SEAT = "Enter a seat number in that row"
 const val LIM_NB_SEATS = 60
 const val PRICE_HIGH = 10
 const val PRICE_LOW = 8
