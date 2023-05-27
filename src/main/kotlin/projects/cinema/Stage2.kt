d025b71cc0fe4d45d96ddf826f00573412908b0b
loicbaillif <larmedeciel@gmail.com>
2023-05-27 22:26:03 +0200

    Kotlin/begin/cinema/st2/v12 Export functions in dedicated file

--- src/main/kotlin/projects/cinema/Stage2.kt
+++ src/main/kotlin/projects/cinema/Stage2.kt
@@ -21,31 +21,3 @@ fun stage2() {
 
     println("$PRINT_INCOME\$${calculateIncome(nbRows, nbSeats)}")
 }
-
-
-fun calculateIncome(nbRows: Int, nbSeats: Int): Int {
-    // Variables
-    val totalSeats = nbRows * nbSeats
-
-    if (totalSeats <= LIM_NB_SEATS) {
-        return totalSeats * PRICE_HIGH
-    } else {
-        val priceHigh = nbRows / 2 * PRICE_HIGH
-        val priceLow = (nbRows / 2 + nbRows % 2) * PRICE_LOW
-        return (priceHigh + priceLow ) * nbSeats
-    }
-}
-
-
-fun checkInputInt(prompt: String, default: Int = 1): Int {
-    // This function print the prompt, then return user input as an Integer
-    // If user input is not an integer, returns default value
-    println(prompt)
-    val userInputString = readln()
-
-    for (ch in userInputString) {
-        if (ch !in '0'..'9') return default
-    }
-
-    return userInputString.toInt()
-}
