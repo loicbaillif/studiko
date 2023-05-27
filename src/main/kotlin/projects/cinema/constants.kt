30967491cde0a28b0aaecdb96c6b60fad2c5e9d2
loicbaillif <larmedeciel@gmail.com>
2023-05-27 23:37:23 +0200

    Kotlin/begin/cinema/st3/v2 Move constants to separated file

---
+++ src/main/kotlin/projects/cinema/constants.kt
@@ -0,0 +1,12 @@
+package projects.cinema
+
+const val ASK_NB_ROWS = "Enter the number of rows:"
+const val ASK_NB_SEATS = "Enter the number of seats in each row:"
+const val LIM_NB_SEATS = 60
+const val PRICE_HIGH = 10
+const val PRICE_LOW = 8
+const val PRINT_INCOME = "Total income:\n"
+
+fun constants() {
+
+}
