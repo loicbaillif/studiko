636cf19a72d6055e9b66f5398219658b55fa1f8f
loicbaillif <larmedeciel@gmail.com>
2023-05-27 20:46:48 +0200

    Kotlin/begin/cinema/st1/v6 Bugs corrected, end of stage 1

--- src/main/kotlin/projects/cinema/stage1.kt
+++ src/main/kotlin/projects/cinema/stage1.kt
@@ -11,13 +11,13 @@ fun stage1() {
     val nbRows = 7
     val nbSeats = 8
 
-    print("  ")
+    print("Cinema:\n  ")
     for (i in 1..nbSeats) {
         print("$i ")
     }
     println()
 
-    for (i in 0..nbRows) {
+    for (i in 1..nbRows) {
 
         for (j in 0..nbSeats) {
             print(if (j == 0) "$i " else "S ")
