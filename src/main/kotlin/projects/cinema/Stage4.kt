967fbbe8b545095518918e036661fa58aa677a2e
loicbaillif <larmedeciel@gmail.com>
2023-05-28 19:06:30 +0200

    Kotlin/begin/cinema/st4/init Stage 4/5: Menu, please!

---
+++ src/main/kotlin/projects/cinema/Stage4.kt
@@ -0,0 +1,12 @@
+package projects.cinema
+
+/**
+ * Stage 4/5: Menu, please
+ * https://hyperskill.org/projects/138/stages/738/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage4() {
+
+}
