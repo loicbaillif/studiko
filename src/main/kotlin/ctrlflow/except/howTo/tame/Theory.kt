8549e60ca4c820b72b39eca225c4d9bc55303cfb
loicbaillif <larmedeciel@gmail.com>
2023-05-31 21:20:02 +0200

    Kotlin/begin/tameExcept/th/v2 Second example: Exception

--- src/main/kotlin/ctrlflow/except/howTo/tame/Theory.kt
+++ src/main/kotlin/ctrlflow/except/howTo/tame/Theory.kt
@@ -12,6 +12,8 @@ fun theory() {
     println("\n*** 1) Another type of exceptions ***")
     println("\t. Item to buy = hamburger - Price = 2 € - Budget = 17 €:")
     println("\t. Money to spend = ${calculateSpentMoney(17, 2)} €")
+    println("\n. Item to buy = sample candy - Price = 0€ - Budget = 13 €:")
+    println("\t. Money to spend = ${calculateSpentMoney(13, 0)}")
 
     println("\n***** End of theory *****")
 }
