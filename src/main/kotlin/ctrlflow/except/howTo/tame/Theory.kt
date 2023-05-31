25fe4c07b1a1a946f7438aef0930487b436a7ca5
loicbaillif <larmedeciel@gmail.com>
2023-05-31 22:18:02 +0200

    Kotlin/begin/tameExcept/th/v6 Second throw example

--- src/main/kotlin/ctrlflow/except/howTo/tame/Theory.kt
+++ src/main/kotlin/ctrlflow/except/howTo/tame/Theory.kt
@@ -26,6 +26,14 @@ fun theory() {
     }
     println("\t. Money to spend = $result €")
 
+    result = try {
+        (calculateSpentMoney(37, -2)).toString()
+    } catch (e: Exception) {
+        "${e.message}"
+    }
+    println("\n\t. Item to buy = hamburger - Price = -2 € - Budget = 37 €:")
+    println("\t. Money to spend = $result €")
+
     println("\n***** End of theory *****")
 }
 
@@ -35,7 +43,7 @@ fun calculateSpentMoney(total: Int, itemPrice: Int): Int {
     if (total < 0) throw Exception("Total cannot be negative")
 
     // Catch a negative itemPrice (another nonsense)
-    if (itemPrice < 0) throw Exception("Item price cannot be negative")
+    if (itemPrice < 0) throw Exception("Error: Item price cannot be negative")
 
     // Catch exception to avoid 0 division
     if (itemPrice == 0) return 0
