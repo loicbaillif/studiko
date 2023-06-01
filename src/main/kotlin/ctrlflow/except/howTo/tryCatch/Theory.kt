d59dabd9e32a3894ae33e0b260a85d6310a4a9df
loicbaillif <larmedeciel@gmail.com>
2023-06-01 22:43:32 +0200

    Kotlin/begin/tryCatch/th/v1 First example

--- src/main/kotlin/ctrlflow/except/howTo/tryCatch/Theory.kt
+++ src/main/kotlin/ctrlflow/except/howTo/tryCatch/Theory.kt
@@ -8,4 +8,19 @@ package ctrlflow.except.howTo.tryCatch
 
 fun theory() {
     println("***** Theory *****")
+
+    println("\n1) *** The try-catch statement ***")
+    val userInput = readln().toInt()
+
+    println("Before the try-catch block")
+
+    try {
+        println("Inside the try block, before an exception")
+        println("2 / $userInput ==> ${2/userInput}")
+        println("Inside the try block, after the possible exception")
+    } catch (e: ArithmeticException) {
+        println("${e.message}: Division by zero!")
+    }
+
+    println("After the try-catch block")
 }
