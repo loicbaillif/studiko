adae0517e3bb5118d7720b373d0ca022c2813b16
loicbaillif <larmedeciel@gmail.com>
2023-06-02 18:51:19 +0200

    Kotlin/begin/tryCatch/th/v2 Get message

--- src/main/kotlin/ctrlflow/except/howTo/tryCatch/Theory.kt
+++ src/main/kotlin/ctrlflow/except/howTo/tryCatch/Theory.kt
@@ -19,7 +19,15 @@ fun theory() {
         println("2 / $userInput ==> ${2/userInput}")
         println("Inside the try block, after the possible exception")
     } catch (e: ArithmeticException) {
-        println("${e.message}: Division by zero!")
+        println("Exception: Division by zero!")
+    }
+
+    println("\n2) Getting info about exceptions")
+    println("\t. Use : \"e.message\"")
+    try {
+        println("20 divided by $userInput ==> ${20/userInput}")
+    } catch (e: ArithmeticException) {
+        println("\t. ${e.message}")
     }
 
     println("After the try-catch block")
