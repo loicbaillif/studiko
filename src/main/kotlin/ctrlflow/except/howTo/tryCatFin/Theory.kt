154f089c503a3c8e7f3162707b430a7d5175ef87
loicbaillif <larmedeciel@gmail.com>
2023-06-02 22:16:35 +0200

    Kotlin/begin/tryCatFin/th/v1 Basic structure

--- src/main/kotlin/ctrlflow/except/howTo/tryCatFin/Theory.kt
+++ src/main/kotlin/ctrlflow/except/howTo/tryCatFin/Theory.kt
@@ -9,4 +9,13 @@ package ctrlflow.except.howTo.tryCatFin
 fun theory() {
     println("***** Theory *****")
 
+    println("\n*** 1) The finally block ***")
+
+    try {
+        // Code which might throw an exception
+    } catch (e: Exception) {
+        // Exception handler
+    } finally {
+        // This code is always executed
+    }
 }
