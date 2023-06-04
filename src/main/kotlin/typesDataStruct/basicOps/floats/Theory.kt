3ced3d4c9b47726f734ada4f0630c7dae85a4ccd
loicbaillif <larmedeciel@gmail.com>
2023-06-04 21:30:33 +0200

    Kotlin/begin/floats/th/v4 Calculus on double

--- src/main/kotlin/typesDataStruct/basicOps/floats/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/floats/Theory.kt
@@ -24,6 +24,13 @@ fun theory() {
     println("\t. value = $someFloat1 - type = ${someFloat1::class.simpleName}")
     println("\t. value = $someFloat2 - type = ${someFloat2::class.simpleName}")
 
+    println("Calculus on Doubles")
+    print("\t. $someDouble1 + $someDouble2 = ${someDouble1 + someDouble2} - ")
+    println(" type = ${(someDouble1 + someDouble2)::class.simpleName}")
+    print("\t. $someDouble1 * $someDouble2 = ${someDouble1 * someDouble2} - ")
+    println(" type = ${(someDouble1 * someDouble2)::class.simpleName}")
+
+
 
     println("\n***** End of Theory *****")
 }
