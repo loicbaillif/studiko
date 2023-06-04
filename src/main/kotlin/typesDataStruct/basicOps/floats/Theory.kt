e10bebecda9af5d31c4e312391ccbe863e54d05e
loicbaillif <larmedeciel@gmail.com>
2023-06-04 21:15:49 +0200

    Kotlin/begin/floats/th/v1 doubles

--- src/main/kotlin/typesDataStruct/basicOps/floats/Theory.kt
+++ src/main/kotlin/typesDataStruct/basicOps/floats/Theory.kt
@@ -9,5 +9,13 @@ package typesDataStruct.basicOps.floats
 fun theory() {
     println("***** Theory *****")
 
+    println("\n*** 1) Floating-point arithmetic operations")
+    val someDouble1 = 1.0
+    val someDouble2 = -3.21
+    val pi = 3.14159
+    println("value = $someDouble1 - type = ${someDouble1::class.simpleName}")
+    println("value = $someDouble2 - type = ${someDouble2::class.simpleName}")
+    println("value = $pi - type = ${pi::class.simpleName}")
+
     println("\n***** End of Theory *****")
 }
