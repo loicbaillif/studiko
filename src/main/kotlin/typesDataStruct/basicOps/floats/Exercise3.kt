a27b773d61190d9caa73f3bcf54da7378be0bd50
loicbaillif <larmedeciel@gmail.com>
2023-06-05 21:52:22 +0200

    Kotlin/begin/floats/ex3/init Evaluate an expression

---
+++ src/main/kotlin/typesDataStruct/basicOps/floats/Exercise3.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.basicOps.floats
+
+/**
+ * https://hyperskill.org/learn/step/4509
+ * Exercise: Evaluate an expression
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: Evaluate an expression ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
