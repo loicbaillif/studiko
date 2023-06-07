7b6f0c5b7104d1e184838a81ca91d2dede020f86
loicbaillif <larmedeciel@gmail.com>
2023-06-07 21:20:55 +0200

    Kotlin/begin/floats/ex5/init Uses the Double in the calculations

---
+++ src/main/kotlin/typesDataStruct/basicOps/floats/Exercise5.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.basicOps.floats
+
+/**
+ * Exercise: Uses the Double in the calculations
+ * https://hyperskill.org/learn/step/4507
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise5() {
+    println("*** Exercise: Uses the Double in the calculations ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/typesDataStruct/basicOps/floats/Index.kt
+++ src/main/kotlin/typesDataStruct/basicOps/floats/Index.kt
@@ -10,5 +10,6 @@ fun index() {
     // exercise1()
     // exercise2()
     // exercise3()
-    exercise4()
+    // exercise4()
+    exercise5()
 }
