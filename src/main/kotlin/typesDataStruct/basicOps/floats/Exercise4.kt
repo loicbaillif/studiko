3d37c5fe38d53abbdb35d8b7ce5d485deea3b0ac
loicbaillif <larmedeciel@gmail.com>
2023-06-07 20:40:58 +0200

    Kotlin/begin/floats/ex4/init Celsius to Fahrenheit

---
+++ src/main/kotlin/typesDataStruct/basicOps/floats/Exercise4.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.basicOps.floats
+
+/**
+ * Exercise: Celsius to Fahrenheit
+ * https://hyperskill.org/learn/step/4512
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise4() {
+    println("*** Exercise: Celsius to Fahrenheit ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
