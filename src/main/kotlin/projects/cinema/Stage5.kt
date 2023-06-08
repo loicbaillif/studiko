e65d3dba52ab5cf0a7de9b7f34a2160a3dee8d6c
loicbaillif <larmedeciel@gmail.com>
2023-06-08 09:49:21 +0200

    Kotlin/begin/cinema/st5/v6 variables moved to dedicated file

--- src/main/kotlin/projects/cinema/Stage5.kt
+++ src/main/kotlin/projects/cinema/Stage5.kt
@@ -10,11 +10,7 @@ package projects.cinema
 fun stage5() {
     // Variables
     val cinemaList = createCinema()
-    var nbTicketsSold = 0
-    var percentage = 0
-    var currentIncome = 0
-    var totalIncome = 0
 
     displayCinemaList(cinemaList) // DEBUG
-    println(String.format(STATISTICS, 3, 3.21, 5, 333)) // DEBUG
+    println(String.format(STATISTICS, nbTicketsSold, percentage, currentIncome, totalIncome)) // DEBUG
 }
---
+++ src/main/kotlin/projects/cinema/variables.kt
@@ -0,0 +1,7 @@
+package projects.cinema
+
+// Used for statistics:
+var nbTicketsSold = 0
+var percentage = 0.0
+var currentIncome = 0
+var totalIncome = 0
