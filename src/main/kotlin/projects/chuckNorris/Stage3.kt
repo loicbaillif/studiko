4932f7536db7f746545dcc39f557f37b06b35ada
loicbaillif <larmedeciel@gmail.com>
2023-06-11 20:47:01 +0200

    Kotlin/begin/chuckNorris/st3/init Stage 3/5: Chuck Norris encrypts only with 0

---
+++ src/main/kotlin/projects/chuckNorris/Stage3.kt
@@ -0,0 +1,12 @@
+package projects.chuckNorris
+
+/**
+ * Stage 3/5: huck Norris encrypts only with zeros
+ * https://hyperskill.org/projects/300/stages/1688/implement#comment
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage3() {
+
+}
