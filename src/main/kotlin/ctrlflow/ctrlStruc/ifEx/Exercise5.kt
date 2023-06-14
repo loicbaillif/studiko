4707539a9460970255d719a4260a9d17b19fc5f1
loicbaillif <larmedeciel@gmail.com>
2023-06-14 06:49:43 +0200

    Kotlin/begin/ifEx/ex5/v4 Print result

--- src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise5.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise5.kt
@@ -14,10 +14,13 @@ fun exercise5() {
     println("*** Exercise: Leap year ***")
 
     // Variable
-    var userInput = readln().toInt()
+    val userInput = readln().toInt()
 
     if (userInput % 4 == 0 && userInput % 100 != 0 || userInput % 400 == 0) {
         // Leap?
+        println(LEAP)
+    } else {
+        println(REGULAR)
     }
 
     println("\n*** End of exercise ***")
