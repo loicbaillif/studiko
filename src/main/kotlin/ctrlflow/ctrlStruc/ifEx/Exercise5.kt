cbe69f9675402aab28dda3fc13bcdf80c3259c51
loicbaillif <larmedeciel@gmail.com>
2023-06-14 06:42:05 +0200

    Kotlin/begin/ifEx/ex5/init Exercise: Leap Year

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/ifEx/Exercise5.kt
@@ -0,0 +1,16 @@
+package ctrlflow.ctrlStruc.ifEx
+
+/**
+ * Exercise: Leap year
+ * https://hyperskill.org/learn/daily/4596
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise5() {
+    println("*** Exercise: Leap year ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
