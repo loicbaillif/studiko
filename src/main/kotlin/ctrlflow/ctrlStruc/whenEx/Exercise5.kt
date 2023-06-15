d09818c375ae8deb8dcde73a25ace548c055e94c
loicbaillif <larmedeciel@gmail.com>
2023-06-15 22:12:16 +0200

    Kotlin/begin/whenEx/ex5/init Shape

---
+++ src/main/kotlin/ctrlflow/ctrlStruc/whenEx/Exercise5.kt
@@ -0,0 +1,16 @@
+package ctrlflow.ctrlStruc.whenEx
+
+/**
+ * Exercise: Shape
+ * https://hyperskill.org/learn/daily/4599
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise5() {
+    println("*** Exercise: Shape ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
