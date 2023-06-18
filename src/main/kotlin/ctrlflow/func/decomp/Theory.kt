ceddb9174284bc73ad588b7043c0d4ef4e04dcd8
loicbaillif <larmedeciel@gmail.com>
2023-06-18 21:24:14 +0200

    Kotlin/begin/decomp/th/v3 Pick user choice

--- src/main/kotlin/ctrlflow/func/decomp/Theory.kt
+++ src/main/kotlin/ctrlflow/func/decomp/Theory.kt
@@ -1,5 +1,7 @@
 package ctrlflow.func.decomp
 
+import java.lang.NumberFormatException
+
 /**
  * Theory: Functional decomposition
  * https://hyperskill.org/learn/step/12668
@@ -10,9 +12,16 @@ fun theory() {
     println("***** Theory *****")
 
     println("\n*** 1) Solving complex tasks ***")
+    chooseAction()
 }
 
 
 fun chooseAction() {
-
+    println("Please choose what you want to do?\n\t1: Control light\n\t2: Control music")
+    var userChoice = 0
+    try {
+        userChoice = readln().toInt()
+    } catch (wrongInput: NumberFormatException) {
+        println("Sorry, wrong input")
+    }
 }
