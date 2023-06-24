cf7905a4b3b30235be6a440afc105bf4c7c85aae
loicbaillif <larmedeciel@gmail.com>
2023-06-24 20:15:36 +0200

    Kotlin/begin/arrays/th/v2 intArrayOf example

--- src/main/kotlin/typesDataStruct/arrays/arrays/Theory.kt
+++ src/main/kotlin/typesDataStruct/arrays/arrays/Theory.kt
@@ -10,6 +10,8 @@ fun theory() {
     println("***** Theory *****")
 
     println("\n*** 1) Creating an array with specified elements ***")
+    val numberArray1 = intArrayOf(1, 2, 3, 4, 5)
+    println(numberArray1.joinToString("\t-\t"))
 
     println("\n***** End of theory *****")
 }
