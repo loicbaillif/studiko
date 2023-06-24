852ed44244b37d4587f3912690ee01e4d2e6945a
loicbaillif <larmedeciel@gmail.com>
2023-06-24 19:05:44 +0200

    Kotlin/begin/arrays/th/init Theory - initialization

---
+++ src/main/kotlin/typesDataStruct/arrays/arrays/Theory.kt
@@ -0,0 +1,15 @@
+package typesDataStruct.arrays.arrays
+
+/**
+ * Theory: Arrays
+ * https://hyperskill.org/learn/step/4564
+ * @author JetBrains Academy
+ */
+
+fun theory() {
+    println("***** Theory *****")
+
+
+
+    println("\n***** End of theory *****")
+}
