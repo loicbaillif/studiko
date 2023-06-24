780b8227ecf29d6fe0dd346d39635d95b351acaa
loicbaillif <larmedeciel@gmail.com>
2023-06-24 21:41:38 +0200

    Kotlin/begin/arrays/th/v3 charArrayOf example

--- src/main/kotlin/typesDataStruct/arrays/arrays/Theory.kt
+++ src/main/kotlin/typesDataStruct/arrays/arrays/Theory.kt
@@ -10,8 +10,13 @@ fun theory() {
     println("***** Theory *****")
 
     println("\n*** 1) Creating an array with specified elements ***")
+    print("Int array example:\n\t. ")
     val numberArray1 = intArrayOf(1, 2, 3, 4, 5)
     println(numberArray1.joinToString("\t-\t"))
+
+    print("\nChar array example:\n\t. ")
+    val charArray1 = charArrayOf('K', 't', 'l')
+    println(charArray1.joinToString("\t-\t"))
 
     println("\n***** End of theory *****")
 }
