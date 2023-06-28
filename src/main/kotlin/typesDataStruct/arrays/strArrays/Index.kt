9265fc96c57eec9b3992332fc0391bea166b817c
loicbaillif <larmedeciel@gmail.com>
2023-06-28 07:19:09 +0200

    Kotlin/begin/strArrays/th/init String arrays theory

--- src/main/kotlin/typesDataStruct/arrays/strArrays/Index.kt
+++ src/main/kotlin/typesDataStruct/arrays/strArrays/Index.kt
@@ -3,7 +3,10 @@ package typesDataStruct.arrays.strArrays
 /**
  * String Arrays
  */
+
 fun index() {
     println("********** String arrays **********\n")
+
+    theory()
 
 }
---
+++ src/main/kotlin/typesDataStruct/arrays/strArrays/Theory.kt
@@ -0,0 +1,11 @@
+package typesDataStruct.arrays.strArrays
+
+/**
+ * Theory: String Arrays
+ * https://hyperskill.org/learn/step/11477
+ * @author JetBrains Academy
+ */
+
+fun theory() {
+    println("***** Theory *****")
+}
