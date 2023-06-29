c0e2e09c45c0010b13eaa40c772b87c54f9ea358
loicbaillif <larmedeciel@gmail.com>
2023-06-29 21:58:49 +0200

    Kotlin/begin/strArrays/ex3/init Guard on the gate

---
+++ src/main/kotlin/typesDataStruct/arrays/strArrays/Exercise3.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.arrays.strArrays
+
+/**
+ * Exercise: Guard on the gate
+ * https://hyperskill.org/learn/step/11488
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: Guard on the gate ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
