4dac963418401b5d5f5fd69aa2438d26bee3737c
loicbaillif <larmedeciel@gmail.com>
2023-06-30 22:00:21 +0200

    Kotlin/begin/mainFun/th/v2 Set back to nominal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 
-fun main(args: Array<String>) {
+fun main() {
     println()
     println("**************************************************")
     println("*** JetBrains Academy: First Glance at Android ***")
     println("**************************************************")
-    println(args[0])
+    println()
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
