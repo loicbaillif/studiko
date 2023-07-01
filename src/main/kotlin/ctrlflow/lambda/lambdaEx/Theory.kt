99ab503a8b646d18db1b1b7f1c8c169a142c294c
loicbaillif <larmedeciel@gmail.com>
2023-07-01 21:48:01 +0200

    Kotlin/begin/lambdaEx/th/v3 Lambdas and syntactic sugar

--- src/main/kotlin/ctrlflow/lambda/lambdaEx/Theory.kt
+++ src/main/kotlin/ctrlflow/lambda/lambdaEx/Theory.kt
@@ -18,6 +18,11 @@ fun theory() {
     println("\t. ${mul2(7, 6)}")
 
 
+    println("\n*** 2) Lambdas and syntactic sugar")
+    val someText1 = "I don't know... What to say..."
+    val someText2 = someText1.filter({ c: Char -> c != '.' })
+    println(someText2)
+
 
     println("\n***** End of Theory *****")
 }
