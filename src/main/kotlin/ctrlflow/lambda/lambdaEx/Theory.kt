df8dd3049b918dadcf9d4222d0e9145d44332566
loicbaillif <larmedeciel@gmail.com>
2023-07-01 21:40:00 +0200

    Kotlin/begin/lambdaEx/th/v2 Example 2

--- src/main/kotlin/ctrlflow/lambda/lambdaEx/Theory.kt
+++ src/main/kotlin/ctrlflow/lambda/lambdaEx/Theory.kt
@@ -12,7 +12,10 @@ fun theory() {
     println("\n*** 1) Functions without names")
 
     val mul1 = fun(a: Int, b: Int): Int { return  a * b }
-    println(mul1(3, 7))
+    println("\t. ${mul1(3, 7)}")
+
+    val mul2 = { a: Int, b: Int -> a * b }
+    println("\t. ${mul2(7, 6)}")
 
 
 
