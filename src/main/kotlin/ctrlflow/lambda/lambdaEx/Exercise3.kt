d20684a76dae94eed30022fc7aabbd8096482fb9
loicbaillif <larmedeciel@gmail.com>
2023-07-02 18:42:37 +0200

    Kotlin/begin/lambdaEx/ex3/init Function composition

---
+++ src/main/kotlin/ctrlflow/lambda/lambdaEx/Exercise3.kt
@@ -0,0 +1,18 @@
+package ctrlflow.lambda.lambdaEx
+
+/**
+ * Exercise: Function composition
+ * https://hyperskill.org/learn/step/6161
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: Function composition ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
+
+
