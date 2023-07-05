bf282ff522afb17ba576ede1d5e9ecc2a8ee870a
loicbaillif <larmedeciel@gmail.com>
2023-07-05 22:25:22 +0200

    Kotlin/begin/readFiles/th/v2 First example

--- src/main/kotlin/addit/files/readFiles/Theory.kt
+++ src/main/kotlin/addit/files/readFiles/Theory.kt
@@ -1,5 +1,7 @@
 package addit.files.readFiles
 
+import java.io.File
+
 /**
  * Theory: Reading Files
  * https://hyperskill.org/learn/step/6351
@@ -10,7 +12,9 @@ fun theory() {
     println("***** Theory *****")
 
     println("\n*** 1) read text")
-
+    val fileName = "src/main/kotlin/addit/files/readFiles/reading.txt"
+    val lines = File(fileName).readText()
+    print(lines)
 
 
     println("\n***** End of Theory *****")
