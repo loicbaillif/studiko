4646d657eee349393cc0a7976744df1fa0e8880d
loicbaillif <larmedeciel@gmail.com>
2023-07-16 09:53:25 +0200

    Kotlin/begin/workWithStr/ex4/init exercise: Working with strings

---
+++ src/main/kotlin/typesDataStruct/strings/workWith/Exercise4.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.strings.workWith
+
+/**
+ * Exercise: The information about a person
+ * https://hyperskill.org/learn/daily/4552
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise4() {
+    println("*** Exercise: The information about a person ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
