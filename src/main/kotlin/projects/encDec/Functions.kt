7eafa50d842be86a3255ed5bf0b1d250beee5292
loicbaillif <larmedeciel@gmail.com>
2023-07-20 22:46:38 +0200

    Kotlin/begin/encDec/st5/v24 cleanArgs signature

--- src/main/kotlin/projects/encDec/Functions.kt
+++ src/main/kotlin/projects/encDec/Functions.kt
@@ -62,6 +62,16 @@ fun cipherStage1(input: String) {
 }
 
 
+fun cleanArgs(args: Array<String>) {
+    // This function will clean the arguments, based on following criterias:
+    //   1. If there is no -mode, the program should work in the enc mode;
+    //   2. If there is no -key, the program should consider that key is 0;
+    //   3. If there is no -data and no -in the program should assume that the data is an empty string;
+    //   4. If there is no -out argument, the program must print data to the standard output;
+    //   5. If there are both -data and -in arguments, your program should prefer -data over -in.
+}
+
+
 fun decode3() {
     // decode function for stage 3
     // Variables
@@ -172,10 +182,6 @@ fun setLaunchArgs5(args: Array<String>): Array<String> {
     val outData = if (outIndex == -1) "" else args[outIndex + 1]
 
     resultArray += arrayOf(inData, outData)
-
-    // Fill default values
-    if (resultArray[0] == "") resultArray[0] = "enc"
-    if (resultArray[1] == "") resultArray[1] = "0"
 
     // Check validity
     if (!checkArgsValid(resultArray)) return arrayOf("false")
