cbc8bcbe321ab0f50cd5406ca8531b2aa1e7cb01
loicbaillif <larmedeciel@gmail.com>
2023-07-20 22:53:41 +0200

    Kotlin/begin/encDec/st5/v26 First test

--- src/main/kotlin/projects/encDec/Index.kt
+++ src/main/kotlin/projects/encDec/Index.kt
@@ -11,7 +11,7 @@ fun index() {
     // stage2()
     // stage3()
     // stage4(arrayOf("-data", "I want to break free", "-key", "5"))
-    stage5(arrayOf("-data", "Pong!", "-key", "5", "-mode", "enc", "-in", "data/textFile1.txt", "-out", "data/temp.txt"))
+    stage5(arrayOf("-data", "Pong!", "-key", "5", "-in", "data/textFile1.txt", "-out", "data/temp.txt"))
 
     println("\n\n********** END OF PROJECT **********")
 }
