37b0d3ecf4ac1c8623e41dbc9913f247679866a5
loicbaillif <larmedeciel@gmail.com>
2023-07-29 05:43:25 +0200

    Kotlin/begin/forArrays/th/v1 Iterating through

--- src/main/kotlin/typesDataStruct/arrays/forArrays/Theory.kt
+++ src/main/kotlin/typesDataStruct/arrays/forArrays/Theory.kt
@@ -9,7 +9,11 @@ package typesDataStruct.arrays.forArrays
 fun theory() {
     println("***** Theory *****")
 
-
+    println("\n*** Iterating through an array")
+    val daysOfWeek = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    for (day in daysOfWeek) {
+        println("\t. $day")
+    }
 
     println("\n***** End of theory *****")
 }
