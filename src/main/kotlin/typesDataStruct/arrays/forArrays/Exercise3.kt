709afa0f869384b5d00bbdafa53f09f4334d2b72
loicbaillif <larmedeciel@gmail.com>
2023-08-01 06:45:57 +0200

    Kotlin/begin/forArrays/ex3/init Triples - init

---
+++ src/main/kotlin/typesDataStruct/arrays/forArrays/Exercise3.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.arrays.forArrays
+
+/**
+ * Exercise: Triples
+ * https://hyperskill.org/learn/step/4705
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: Triples ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
