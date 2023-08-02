799a2ad3ad8212ba8880f17ad9dd88e17a05b2fc
loicbaillif <larmedeciel@gmail.com>
2023-08-02 04:42:17 +0200

    Kotlin/begin/forArrays/ex4/init Exercise: Is it here?

---
+++ src/main/kotlin/typesDataStruct/arrays/forArrays/Exercise4.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.arrays.forArrays
+
+/**
+ * Exercise: Is it here?
+ * https://hyperskill.org/learn/step/4696
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise4() {
+    println("*** Exercise: Is it here? ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
