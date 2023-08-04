e6ed39368f0c813f935de47b1bbe181ad4863917
loicbaillif <larmedeciel@gmail.com>
2023-08-04 19:21:10 +0200

    Kotlin/begin/multiDim/ex3/init Exercise: Corners

---
+++ src/main/kotlin/typesDataStruct/arrays/multiDim/Exercise3.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.arrays.multiDim
+
+/**
+ * Exercise:Corners
+ * https://hyperskill.org/learn/step/10956
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: Corners ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
