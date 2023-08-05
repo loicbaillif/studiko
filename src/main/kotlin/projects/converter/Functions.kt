b995e53587add01e1c7bcac722ae7443e54ac41c
loicbaillif <larmedeciel@gmail.com>
2023-08-05 13:43:14 +0200

    Kotlin/begin/converter/st1/v4 loop skeleton

--- src/main/kotlin/projects/converter/Functions.kt
+++ src/main/kotlin/projects/converter/Functions.kt
@@ -3,5 +3,10 @@ package projects.converter
 fun tenToBin(input: Int): String {
     // Variable
     val sbResult = StringBuilder()
+    var tempInput = input
 
+    // Loop
+    while (tempInput > 0) {
+
+    }
 }
