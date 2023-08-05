e0c323ddcdef05f4e3d17b95e5d2136da4a3e53f
loicbaillif <larmedeciel@gmail.com>
2023-08-05 13:16:33 +0200

    Kotlin/begin/converter/st1/v1 Text prompts defined

---
+++ src/main/kotlin/projects/converter/Constants.kt
@@ -0,0 +1,5 @@
+package projects.converter
+
+const val CONVERSION_RESULT = "Conversion result:"
+const val INPUT_DECIMAL = "Enter number in decimal system:"
+const val TARGET_BASE = "Enter target base:"
