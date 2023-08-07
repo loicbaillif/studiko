20953a4f3caea134dced4332246d136c61077570
loicbaillif <larmedeciel@gmail.com>
2023-08-07 08:01:03 +0200

    Kotlin/begin/decClass/th/v2 conventional version

--- src/main/kotlin/oop/basics/decClass/Theory.kt
+++ src/main/kotlin/oop/basics/decClass/Theory.kt
@@ -10,7 +10,7 @@ fun theory() {
     println("***** Theory *****")
 
     println("\n*** 1) Object creation")
-    val object1: thClass = thClass()
+    val object1 = thClass()
 
     println("\n***** End of theory *****")
 }
