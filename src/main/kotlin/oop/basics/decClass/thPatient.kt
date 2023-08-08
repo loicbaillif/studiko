b09d57186cb33f3e25e932a205af53fd80e3d891
loicbaillif <larmedeciel@gmail.com>
2023-08-08 19:31:44 +0200

    Kotlin/begin/decClass/th/v3 Patient class with variables

---
+++ src/main/kotlin/oop/basics/decClass/thPatient.kt
@@ -0,0 +1,11 @@
+package oop.basics.decClass
+
+/**
+ * Patient class used for theory only
+ */
+
+class thPatient {
+    var name: String = "John Doe"
+    var age: Int = 0
+    var height: Double = 0.0
+}
