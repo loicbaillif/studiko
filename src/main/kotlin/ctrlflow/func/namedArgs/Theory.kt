22e77074a1f5751e7ce1dc09d71fb6978fbb1da5
loicbaillif <larmedeciel@gmail.com>
2023-08-12 11:54:02 +0200

    Kotlin/begin/namedArgs/th/v2 Usage 2

--- src/main/kotlin/ctrlflow/func/namedArgs/Theory.kt
+++ src/main/kotlin/ctrlflow/func/namedArgs/Theory.kt
@@ -11,6 +11,11 @@ fun theory() {
 
     println("\n*** 1) Improving code readability")
     val amount1 = calcEndDayAmount(1000, 10, 500)
+    val amount2 = calcEndDayAmount(
+        startCash = 1000,
+        ticketPrice =  10,
+        nbTickets = 500
+    )
 
 
     println("\n***** End of theory *****")
