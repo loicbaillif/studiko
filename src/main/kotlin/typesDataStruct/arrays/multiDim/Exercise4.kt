5ff934c311cd31b2d6fbef27bf92e5869070c426
loicbaillif <larmedeciel@gmail.com>
2023-08-16 22:53:57 +0200

    Kotlin/begin/multiDim/ex4/v1 Initial code from author

--- src/main/kotlin/typesDataStruct/arrays/multiDim/Exercise4.kt
+++ src/main/kotlin/typesDataStruct/arrays/multiDim/Exercise4.kt
@@ -10,7 +10,14 @@ package typesDataStruct.arrays.multiDim
 fun exercise4() {
     println("*** Exercise: Shapeshifter ***")
 
-
+    //Do not touch code below
+    var inputArray: Array<Array<String>> = arrayOf()
+    val n = readLine()!!.toInt()
+    for (i in 0 until n) {
+        val strings = readLine()!!.split(' ').toTypedArray()
+        inputArray += strings
+    }
+    // write your code here
 
     println("\n*** End of exercise ***")
 }
