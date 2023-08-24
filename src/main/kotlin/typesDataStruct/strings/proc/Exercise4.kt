2acb45310a99a1b6a57bf3105d3866baf446f695
loicbaillif <larmedeciel@gmail.com>
2023-08-24 20:46:13 +0200

    Kotlin/begin/procStr/ex4/init Exercise: Double characters

---
+++ src/main/kotlin/typesDataStruct/strings/proc/Exercise4.kt
@@ -0,0 +1,17 @@
+package typesDataStruct.strings.proc
+
+/**
+ * Exercise: Double characters
+ * https://hyperskill.org/learn/daily/9040
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+
+fun exercise4() {
+    println("*** Exercise: Double characters ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/typesDataStruct/strings/proc/Index.kt
+++ src/main/kotlin/typesDataStruct/strings/proc/Index.kt
@@ -8,5 +8,6 @@ fun index() {
     // theory()
     // exercise1()
     // exercise2()
-    exercise3()
+    // exercise3()
+    exercise4()
 }
