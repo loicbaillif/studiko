b01f273ea0e963878840942e117beeca69c29205
loicbaillif <larmedeciel@gmail.com>
2023-08-26 14:01:48 +0200

    Kotlin/begin/declarFun/ex5/init The sum of three numbers

---
+++ src/main/kotlin/ctrlflow/func/declarFun/Exercise5.kt
@@ -0,0 +1,16 @@
+package ctrlflow.func.declarFun
+
+/**
+ * Exercise: The sum of three numbers
+ * https://hyperskill.org/learn/daily/4584
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise5() {
+    println("*** Exercise: The sum of three ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
