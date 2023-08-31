0d4c2c0cdc8c11f020b807d7da5a8b11f1c32910
loicbaillif <larmedeciel@gmail.com>
2023-08-31 07:06:00 +0200

    Kotlin/begin/whenEx/ex7/v4 case "six" to "nine"

--- src/main/kotlin/ctrlflow/ctrlStruc/whenEx/Exercise7.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/whenEx/Exercise7.kt
@@ -17,6 +17,10 @@ fun exercise7() {
         "three" -> 3
         "four" -> 4
         "five" -> 5
+        "six" -> 6
+        "seven" -> 7
+        "eight" -> 8
+        "nine" -> 9
         else -> 0
     })
 
