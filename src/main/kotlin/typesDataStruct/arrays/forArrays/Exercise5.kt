1ad1e04b9b456dcf9992e9ba09fdb4fb1ce42306
loicbaillif <larmedeciel@gmail.com>
2023-09-04 09:06:27 +0200

    Kotlin/begin/forArrays/ex5/init Exercise: Find the max

---
+++ src/main/kotlin/typesDataStruct/arrays/forArrays/Exercise5.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.arrays.forArrays
+
+/**
+ * Exercise: Find the max
+ * https://hyperskill.org/learn/step/4703
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise5() {
+    println("*** Exercise: Find the max ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
