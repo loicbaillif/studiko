3e4dca69034b4ca381c513a297080ea9556ee01f
loicbaillif <larmedeciel@gmail.com>
2023-09-12 20:39:54 +0200

    Kotlin/begin/debugTech/th/v6 Define class for assertions example

--- src/main/kotlin/addit/errorless/debug/Theory.kt
+++ src/main/kotlin/addit/errorless/debug/Theory.kt
@@ -12,6 +12,9 @@ fun theory() {
     println("\n*** 1) Logging/println technique")
     count(1, 10)
 
+
+    println("\n*** 2) Assertions")
+
     println("\n***** End of theory *****")
 }
 
@@ -24,4 +27,14 @@ fun count(start: Int, to: Int) {
         start++
     }
     println("count() ended")
+}
+
+
+class Cat(val name: String, val age: Int) {
+    val enoughCat = false // Of course, it's a false, there are never enough cats!
+    init {
+        check(!enoughCat) { "You cannot add a new cat" } // IllegalStateException
+        require(age >= 0) { "Invalid age: $age" }        // IllegalArgumentException
+        require(name.isNotEmpty()) { "You should pick a name" }
+    }
 }
