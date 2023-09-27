091890edb23ed57f93b35fc608c7f9ee1040fd47
loicbaillif <larmedeciel@gmail.com>
2023-09-27 21:35:50 +0200

    Kotlin/begin/jumpRet/ex5/v1 Get user input

--- src/main/kotlin/ctrlflow/ctrlStruc/jumpRet/Exercise5.kt
+++ src/main/kotlin/ctrlflow/ctrlStruc/jumpRet/Exercise5.kt
@@ -10,6 +10,7 @@ package ctrlflow.ctrlStruc.jumpRet
 fun exercise5() {
     println("*** Exercise: Unique characters ***")
 
+    val userInput = readln()
 
 
     println("\n*** End of exercise ***")
