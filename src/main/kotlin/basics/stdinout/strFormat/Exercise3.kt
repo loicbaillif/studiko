c0d3a31cc085450497ed9d761a89dcb558e7048e
loicbaillif <larmedeciel@gmail.com>
2023-10-01 16:13:43 +0200

    Kotlin/begin/strFormat/ex3/v4 Take width into account

--- src/main/kotlin/basics/stdinout/strFormat/Exercise3.kt
+++ src/main/kotlin/basics/stdinout/strFormat/Exercise3.kt
@@ -17,5 +17,5 @@ fun exercise3() {
 
 
 fun doubleFormat(value: Double, width: Int, precision: Int): String {
-    return String.format("%.${precision}f", value)
+    return String.format("%${width}.${precision}f", value)
 }
