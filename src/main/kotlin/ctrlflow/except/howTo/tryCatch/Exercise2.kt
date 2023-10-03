1078ad28b59278ce5e0bfc1d41011e843b78be38
loicbaillif <larmedeciel@gmail.com>
2023-10-03 20:30:25 +0200

    Kotlin/begin/tryCatch/ex2/v3 Add exception output

--- src/main/kotlin/ctrlflow/except/howTo/tryCatch/Exercise2.kt
+++ src/main/kotlin/ctrlflow/except/howTo/tryCatch/Exercise2.kt
@@ -20,6 +20,6 @@ fun printFifthCharacter(input: String): String {
     try {
         return "The fifth character of the entered word is ${input[4]}"
     } catch (e: StringIndexOutOfBoundsException) {
-
+        return "The input word is too short!"
     }
 }
