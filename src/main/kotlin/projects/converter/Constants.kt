baa068c64e5972d19b5b7e33327b73a90da4219a
loicbaillif <larmedeciel@gmail.com>
2023-10-05 22:01:25 +0200

    Kotlin/begin/converter/st2/v2 Other constants defined

--- src/main/kotlin/projects/converter/Constants.kt
+++ src/main/kotlin/projects/converter/Constants.kt
@@ -1,6 +1,9 @@
 package projects.converter
 
 const val CONVERSION_RESULT = "Conversion result:"
+const val CONVERSION_RESULT_TO_10 = "Conversion to decimal result:"
 const val INPUT_DECIMAL = "Enter number in decimal system:"
 const val PROMPT_STAGE2 = "Do you want to convert /from decimal or /to decimal? (To quit type /exit)"
+const val SOURCE_NUMBER_OTHER_BASE = "Enter source number:"
+const val SOURCE_OTHER_BASE = "Enter source base:"
 const val TARGET_BASE = "Enter target base:"
