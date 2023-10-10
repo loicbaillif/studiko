6b699ee6b4e818ed7f83e60a34abfad51293ba32
loicbaillif <larmedeciel@gmail.com>
2023-10-10 20:41:08 +0200

    Kotlin/begin/bigInt/th/v1 Overflow example

--- src/main/kotlin/addit/stdLib/bigInt/Theory.kt
+++ src/main/kotlin/addit/stdLib/bigInt/Theory.kt
@@ -9,7 +9,8 @@ package addit.stdLib.bigInt
 fun theory() {
     println("***** Theory *****")
 
-
+    val maxInt = Int.MAX_VALUE
+    println("maxInt = $maxInt\t\t;\tmaxInt + 1 = ${maxInt + 1}")
 
     println("\n***** End of theory *****")
 }
