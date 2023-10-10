bef5cf321f910a03857b20cca78ff4851db3a1d0
loicbaillif <larmedeciel@gmail.com>
2023-10-10 20:44:08 +0200

    Kotlin/begin/bigInt/th/v2 First BigInteger

--- src/main/kotlin/addit/stdLib/bigInt/Theory.kt
+++ src/main/kotlin/addit/stdLib/bigInt/Theory.kt
@@ -1,5 +1,7 @@
 package addit.stdLib.bigInt
 
+import java.math.BigInteger
+
 /**
  * Theory: BigInteger
  * https://hyperskill.org/learn/step/7964
@@ -9,8 +11,14 @@ package addit.stdLib.bigInt
 fun theory() {
     println("***** Theory *****")
 
+    println("\n*** 1) Overflow example")
+
     val maxInt = Int.MAX_VALUE
     println("maxInt = $maxInt\t\t;\tmaxInt + 1 = ${maxInt + 1}")
+
+
+    println("\n*** 2) Creating a BigInteger object")
+    val someBigInt = BigInteger("12345678910")
 
     println("\n***** End of theory *****")
 }
