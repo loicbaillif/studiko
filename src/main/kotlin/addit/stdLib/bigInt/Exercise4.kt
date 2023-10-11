38967300c859d6f4b8d68e31546d0f49d24563b1
loicbaillif <larmedeciel@gmail.com>
2023-10-11 22:11:09 +0200

    Kotlin/begin/bigInt/ex4/init Exercise: Percentage

---
+++ src/main/kotlin/addit/stdLib/bigInt/Exercise4.kt
@@ -0,0 +1,16 @@
+package addit.stdLib.bigInt
+
+/**
+ * Exercise: Percentage
+ * https://hyperskill.org/learn/step/7971
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise4() {
+    println("*** Exercise: Percentage ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
