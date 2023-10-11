f57f3ae22449f8d07ec321fe1cb2dc3f6b1b3533
loicbaillif <larmedeciel@gmail.com>
2023-10-11 19:54:42 +0200

    Kotlin/begin/arrays/ex2/v2 Set specific values as requested

--- src/main/kotlin/typesDataStruct/arrays/arrays/Exercise2.kt
+++ src/main/kotlin/typesDataStruct/arrays/arrays/Exercise2.kt
@@ -11,6 +11,9 @@ fun exercise2() {
     println("*** Exercise: Initializing an array ***")
 
     val numberArray1 = IntArray(100)
+    numberArray1[0] = 1
+    numberArray1[9] = 10
+    numberArray1[99] = 100
 
     println("\n*** End of exercise ***")
 }
