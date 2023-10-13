8c3ab6f4d4ea29601d9207a409c850b70f6a80bc
loicbaillif <larmedeciel@gmail.com>
2023-10-13 20:45:27 +0200

    Kotlin/begin/converter/st3/v1 Define prompts

--- src/main/kotlin/projects/converter/Constants.kt
+++ src/main/kotlin/projects/converter/Constants.kt
@@ -3,6 +3,8 @@ package projects.converter
 const val CONVERSION_RESULT = "Conversion result:"
 const val CONVERSION_RESULT_TO_10 = "Conversion to decimal result:"
 const val INPUT_DECIMAL = "Enter number in decimal system:"
+const val INPUT_STAGE3 = "Enter number in base %s to convert to base %s (To go back type /back)"
+const val MAIN_STAGE3 = "Enter two numbers in format: {source base} {target base} (To quit type /exit)"
 const val PROMPT_STAGE2 = "Do you want to convert /from decimal or /to decimal? (To quit type /exit)"
 const val SOURCE_NUMBER_OTHER_BASE = "Enter source number:"
 const val SOURCE_OTHER_BASE = "Enter source base:"
