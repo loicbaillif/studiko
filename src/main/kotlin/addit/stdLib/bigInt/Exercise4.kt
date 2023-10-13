100c973b4790df8610e3a2a221f16dd92ef509b2
loicbaillif <larmedeciel@gmail.com>
2023-10-13 07:20:55 +0200

    Kotlin/begin/bigInt/ex4/v3 Calculate percentages

--- src/main/kotlin/addit/stdLib/bigInt/Exercise4.kt
+++ src/main/kotlin/addit/stdLib/bigInt/Exercise4.kt
@@ -1,5 +1,7 @@
 package addit.stdLib.bigInt
 
+import java.math.BigInteger
+
 /**
  * Exercise: Percentage
  * https://hyperskill.org/learn/step/7971
@@ -13,6 +15,9 @@ fun exercise4() {
     val a = readln().toBigInteger()
     val b = readln().toBigInteger()
     val sum = a + b
+    val hundred = BigInteger(100)
+    val per1 = a * hundred / sum
+    val per2 = b * hundred / sum
 
     println("\n*** End of exercise ***")
 }
