efb1db7b131ac95fa736b59114f94815c3a69b26
loicbaillif <larmedeciel@gmail.com>
2023-10-16 20:36:12 +0200

    Kotlin/begin/bigDec/th/v1 From string and user input

--- src/main/kotlin/addit/stdLib/bigDec/Theory.kt
+++ src/main/kotlin/addit/stdLib/bigDec/Theory.kt
@@ -1,5 +1,7 @@
 package addit.stdLib.bigDec
 
+import java.math.BigDecimal
+
 /**
  * Theory: BigDecimal
  * https://hyperskill.org/learn/step/12536
@@ -9,6 +11,11 @@ package addit.stdLib.bigDec
 fun theory() {
     println("***** Theory *****")
 
+    println("\n*** 1) Creating objects of BigDecimal")
+    val bigDecimal1 = BigDecimal("123456789.135792468") // From String
+    val bigDecimal2 = BigDecimal(readln()) // From user input
+    println("\tBigDecimal1 = $bigDecimal1")
+    println("\tBigDecimal2 = $bigDecimal2")
 
 
     println("\n***** End of Theory *****")
