d1a825a1b2fc6c49b755c623ab2a35f6255499f2
loicbaillif <larmedeciel@gmail.com>
2023-10-27 13:55:57 +0200

    Kotlin/begin/bigDec/ex2/init Exercise: Gold mining

---
+++ src/main/kotlin/addit/stdLib/bigDec/Exercise2.kt
@@ -0,0 +1,16 @@
+package addit.stdLib.bigDec
+
+/**
+ * Exercise: Gold mining
+ * https://hyperskill.org/learn/step/14619
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise2() {
+    println("*** Exercise: Gold mining ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/addit/stdLib/bigDec/Index.kt
+++ src/main/kotlin/addit/stdLib/bigDec/Index.kt
@@ -8,5 +8,6 @@ fun index() {
     println("********** BigDecimal **********\n")
 
     // theory()
-    exercise1()
+    // exercise1()
+    exercise2()
 }
