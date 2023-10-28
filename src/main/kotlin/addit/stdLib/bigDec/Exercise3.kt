7c96861dd9cccc4d0ddfca911185fbfe7f088a49
loicbaillif <larmedeciel@gmail.com>
2023-10-28 17:12:26 +0200

    Kotlin/begin/bigDec/ex3/init Exercise: Rounding with a given precision

---
+++ src/main/kotlin/addit/stdLib/bigDec/Exercise3.kt
@@ -0,0 +1,16 @@
+package addit.stdLib.bigDec
+
+/**
+ * Exercise: Rounding with a given precision
+ * https://hyperskill.org/learn/step/12542
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: Rounding with a given precision ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
