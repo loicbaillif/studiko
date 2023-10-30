991d3a5c86678d8021440d1d36ca7bf6854acec1
loicbaillif <larmedeciel@gmail.com>
2023-10-30 20:59:22 +0100

    Kotlin/begin/bigDec/ex5/init Exercise: Multiplication - init

---
+++ src/main/kotlin/addit/stdLib/bigDec/Exercise5.kt
@@ -0,0 +1,16 @@
+package addit.stdLib.bigDec
+
+/**
+ * Exercise: Multiplication
+ * https://hyperskill.org/learn/step/12540
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise5() {
+    println("*** Exercise: Multiplication ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
