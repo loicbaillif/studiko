e3dfab9cdb6f5378868c424218fd00ee8d0fee01
loicbaillif <larmedeciel@gmail.com>
2023-10-30 20:40:39 +0100

    Kotlin/begin/bigDec/ex4/init Exercise: Average - init

---
+++ src/main/kotlin/addit/stdLib/bigDec/Exercise4.kt
@@ -0,0 +1,16 @@
+package addit.stdLib.bigDec
+
+/**
+ * Exercise: Average
+ * https://hyperskill.org/learn/step/12541
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise4() {
+    println("*** Exercise: Average ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
