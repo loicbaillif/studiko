19c36c4a061b7d71fb887b75399e722a47816cf5
loicbaillif <larmedeciel@gmail.com>
2023-11-18 13:57:04 +0100

    Kotlin/begin/random/th/v2 Custom ranges

--- src/main/kotlin/addit/stdLib/random/Theory.kt
+++ src/main/kotlin/addit/stdLib/random/Theory.kt
@@ -6,11 +6,23 @@ fun theory() {
     println("***** Theory *****")
 
     println("\n*** 1) Types of random numbers")
-    println("\tRandom.nextInt() = ${Random.nextInt()}")
-    println("\tRandom.nextLong() = ${Random.nextLong()}")
-    println("\tRandom.nextFloat() = ${Random.nextFloat()}")
-    println("\tRandom.nextDouble() = ${Random.nextDouble()}")
-    println("\tRandom.nextDouble() = ${Random.nextDouble()}")
+    println("\t. Random.nextInt() = ${Random.nextInt()}")
+    println("\t. Random.nextLong() = ${Random.nextLong()}")
+    println("\t. Random.nextFloat() = ${Random.nextFloat()}")
+    println("\t. Random.nextDouble() = ${Random.nextDouble()}")
+    println("\t. Random.nextDouble() = ${Random.nextDouble()}")
+
+
+    println("\n*** 2) Custom Ranges")
+    println("\t. Random.nextInt(100) = ${Random.nextInt(100)}") // int in [0, 100[
+    println("\t. Random.nextInt(10, 100) = ${Random.nextInt(10, 100)}") // int in [10, 100[
+
+    println("\n\t. Random.nextLong(100) = ${Random.nextLong(100)}") // long in [0, 100[
+    println("\t. Random.nextLong(10, 100) = ${Random.nextLong(10, 100)}") // long in [10, 100[
+
+    println("\n\t. Random.nextDouble(5.0) = ${Random.nextDouble(5.0)}") // Double in [0.0, 5.0[
+    println("\t. Random.nextDouble(0.0, 5.0) = ${Random.nextDouble(0.0, 5.0)}") // Double in [0.0, 5.0[
+
 
 
 
