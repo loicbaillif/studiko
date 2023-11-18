4d7a775bc2e01a9c17f0300e5f6340283392bf35
loicbaillif <larmedeciel@gmail.com>
2023-11-18 13:40:53 +0100

    Kotlin/begin/random/th/v1 Different Types

--- src/main/kotlin/addit/stdLib/random/Theory.kt
+++ src/main/kotlin/addit/stdLib/random/Theory.kt
@@ -1,8 +1,16 @@
 package addit.stdLib.random
 
+import kotlin.random.Random
+
 fun theory() {
     println("***** Theory *****")
 
+    println("\n*** 1) Types of random numbers")
+    println("\tRandom.nextInt() = ${Random.nextInt()}")
+    println("\tRandom.nextLong() = ${Random.nextLong()}")
+    println("\tRandom.nextFloat() = ${Random.nextFloat()}")
+    println("\tRandom.nextDouble() = ${Random.nextDouble()}")
+    println("\tRandom.nextDouble() = ${Random.nextDouble()}")
 
 
 
