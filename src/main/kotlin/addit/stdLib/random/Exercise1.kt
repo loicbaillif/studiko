9ed62be178ef94df712601f589190e3d42243aec
loicbaillif <larmedeciel@gmail.com>
2023-11-18 14:06:45 +0100

    Kotlin/begin/random/ex1/init Exercise: RPG dices

---
+++ src/main/kotlin/addit/stdLib/random/Exercise1.kt
@@ -0,0 +1,15 @@
+package addit.stdLib.random
+
+/**
+ * RPG dices
+ * https://hyperskill.org/learn/step/27075
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise1() {
+    println("*** Exercise: RPG dices ***")
+
+    println("\n*** End of exercise ***")
+}
+
--- src/main/kotlin/addit/stdLib/random/Index.kt
+++ src/main/kotlin/addit/stdLib/random/Index.kt
@@ -3,5 +3,6 @@ package addit.stdLib.random
 fun index() {
     println("********** Random **********\n")
 
-    theory()
+    // theory()
+    exercise1()
 }
