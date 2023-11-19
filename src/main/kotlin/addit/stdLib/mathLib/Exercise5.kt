094edcb6c7b55d5949094f3c0f8de1aa8bd2d3e1
loicbaillif <larmedeciel@gmail.com>
2023-11-19 20:15:36 +0100

    Kotlin/begin/mathLib/ex5/v2 Get max of the 2 pairs

--- src/main/kotlin/addit/stdLib/mathLib/Exercise5.kt
+++ src/main/kotlin/addit/stdLib/mathLib/Exercise5.kt
@@ -1,5 +1,7 @@
 package addit.stdLib.mathLib
 
+import kotlin.math.max
+
 /**
  * Exercise: Max of four numbers
  * https://hyperskill.org/learn/step/4621
@@ -11,6 +13,8 @@ fun exercise5() {
     println("*** Exercise: Max of four numbers ***")
 
     val (a, b, c, d) = List(4) { readln().toInt() }
+    val max1 = max(a, b)
+    val max2 = max(c, d)
 
     println("\n*** End of exercise ***")
 }
