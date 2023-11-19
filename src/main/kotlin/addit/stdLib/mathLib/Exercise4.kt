4e06a21e456c2eb3253c2534cf11619474a77c4d
loicbaillif <larmedeciel@gmail.com>
2023-11-19 18:03:08 +0100

    Kotlin/begin/mathLib/ex4/init Exercise: pow - init

---
+++ src/main/kotlin/addit/stdLib/mathLib/Exercise4.kt
@@ -0,0 +1,14 @@
+package addit.stdLib.mathLib
+
+/**
+ * Exercise: Pow
+ * https://hyperskill.org/learn/step/4560
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise4() {
+    println("*** Exercise: pow ***")
+
+    println("\n*** End of exercise ***")
+}
