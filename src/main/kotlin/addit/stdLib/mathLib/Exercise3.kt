d54225667667a77788776005c55d5aa658224b70
loicbaillif <larmedeciel@gmail.com>
2023-11-19 16:44:21 +0100

    Kotlin/begin/mathLib/ex3/init Exercise: Heron's formula - init

---
+++ src/main/kotlin/addit/stdLib/mathLib/Exercise3.kt
@@ -0,0 +1,16 @@
+package addit.stdLib.mathLib
+
+/**
+ * Exercise: Heron's Formula
+ * https://hyperskill.org/learn/step/4562
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: Heron's formula ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
