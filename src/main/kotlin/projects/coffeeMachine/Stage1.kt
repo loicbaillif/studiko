844d3015c6eb914495e69897e6430005e9ab6b9b
loicbaillif <larmedeciel@gmail.com>
2023-11-23 07:59:29 +0100

    Kotlin/begin/coffeeMachine/st1/v1 First proposition

--- src/main/kotlin/projects/coffeeMachine/Stage1.kt
+++ src/main/kotlin/projects/coffeeMachine/Stage1.kt
@@ -8,5 +8,13 @@ package projects.coffeeMachine
  */
 
 fun stage1() {
-
+    println("""
+        Starting to make a coffee
+        Grinding coffee beans
+        Boiling water
+        Mixing boiled water with crushed coffee beans
+        Pouring coffee into the cup
+        Pouring some milk into the cup
+        Coffee is ready!
+    """.trimIndent())
 }
