f36090d2e2ebe2bbd257afa708c4de1cd11fa0ba
loicbaillif <larmedeciel@gmail.com>
2023-11-23 20:47:20 +0100

    Kotlin/begin/coffeeMachine/st2/v2 Define constant integers

--- src/main/kotlin/projects/coffeeMachine/cosntants.kt
+++ src/main/kotlin/projects/coffeeMachine/cosntants.kt
@@ -1,5 +1,12 @@
 package projects.coffeeMachine
 
+// CONSTANT INTEGERS
+const val COFFEE_BEANS_PER_COFFEE = 15
+const val MILK_PER_COFFEE = 50
+const val WATER_PER_COFFEE = 200
+
+
+// CONSTANT STRINGS
 const val QUANTITY_NB_CUPS = """
     For %d cups of coffee you will need:
     %d ml of water
