b108eae286af2dd9b0dc7fba7048c602cb9c51aa
loicbaillif <larmedeciel@gmail.com>
2023-11-23 20:43:17 +0100

    Kotlin/begin/coffeeMachine/st2/v1 Define constant sentences

---
+++ src/main/kotlin/projects/coffeeMachine/cosntants.kt
@@ -0,0 +1,9 @@
+package projects.coffeeMachine
+
+const val QUANTITY_NB_CUPS = """
+    For %d cups of coffee you will need:
+    %d ml of water
+    %d ml of milk
+    %d g of coffee beans
+"""
+const val PROMPT_NB_CUPS = "Write how many cups of coffee you will need:"
