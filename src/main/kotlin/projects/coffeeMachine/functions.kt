0f0e86c6704cbd34746e7598c2875376ef5e2753
loicbaillif <larmedeciel@gmail.com>
2023-11-23 21:21:25 +0100

    Kotlin/begin/coffeeMachine/st2/v5 Define variables for printCoffeeQty

--- src/main/kotlin/projects/coffeeMachine/functions.kt
+++ src/main/kotlin/projects/coffeeMachine/functions.kt
@@ -4,4 +4,9 @@ fun printCoffeeQty (nbCups: Int): Unit {
     /**
      * Print the required quantity of each ingredient for nbCups of coffee
      */
+
+    // Variables
+    val qtyBeans = nbCups * COFFEE_BEANS_PER_COFFEE
+    val qtyMilk = nbCups * MILK_PER_COFFEE
+    val qtyWater = nbCups * WATER_PER_COFFEE
 }
