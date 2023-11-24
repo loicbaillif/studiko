ee7a161f36e429e6458056a6d4bdfe7313c32547
loicbaillif <larmedeciel@gmail.com>
2023-11-24 07:38:46 +0100

    Kotlin/begin/coffeeMachine/st3/init Stage 3/6: Estimate the number of servings

---
+++ src/main/kotlin/projects/coffeeMachine/Stage3.kt
@@ -0,0 +1,12 @@
+package projects.coffeeMachine
+
+/**
+ * Stage 3/6: Estimate the number of servings
+ * https://hyperskill.org/projects/67/stages/362/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage3() {
+
+}
