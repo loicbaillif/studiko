ca12ce35fb48c1dce31180b18330d597482578bc
loicbaillif <larmedeciel@gmail.com>
2023-11-30 19:45:49 +0100

    Kotlin/begin/memberFunc/ex2/init Exercise: Volume of a cube - init

---
+++ src/main/kotlin/oop/basics/memberFunc/Exercise2.kt
@@ -0,0 +1,22 @@
+package oop.basics.memberFunc
+
+/**
+ * Exercise: Volume of a cube
+ * https://hyperskill.org/learn/step/6369
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise2() {
+    println("*** Exercise: Volume of a cube ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
+
+
+class Box(var height: Double, var width: Double, var length: Double) {
+
+    // write the getVolume method here
+}
