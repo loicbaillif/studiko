f63bd8b34fb4902b3c66dd47d56638a9ed3714e0
loicbaillif <larmedeciel@gmail.com>
2023-12-02 10:29:40 +0100

    Kotlin/begin/enum/th/v1 First enums

--- src/main/kotlin/oop/usage/enums/Theory.kt
+++ src/main/kotlin/oop/usage/enums/Theory.kt
@@ -9,7 +9,22 @@ package oop.usage.enums
 fun theory() {
     println("***** Theory *****")
 
-
+    println("\n*** 1) Basic enum")
 
     println("\n***** End of Theory *****")
+}
+
+
+enum class Rainbow {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+}
+
+
+enum class Status {
+    OPEN, PENDING, IN_PROGRESS, RESOLVED, REJECTED, CLOSED
+}
+
+
+enum class Materials {
+    CONCRETE, GLASS, WOOD, FABRIC, METAL, PLASTIC, ROCK
 }
