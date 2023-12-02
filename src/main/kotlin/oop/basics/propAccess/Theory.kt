d3d0a274fc372390e984a9db144214541a7ab5eb
loicbaillif <larmedeciel@gmail.com>
2023-12-02 15:44:00 +0100

    Kotlin/begin/propAccess/th/v4 backing property

--- src/main/kotlin/oop/basics/propAccess/Theory.kt
+++ src/main/kotlin/oop/basics/propAccess/Theory.kt
@@ -12,6 +12,9 @@ fun theory() {
     println("*** 1) Property getter")
     val client1 = Client1()
     println(client1.name)
+    val repository1 = IntegerRepository1()
+    // repository1.list.add(33) // Generates an error, Unresolved reference
+    for (elt in repository1.list) println("\t$elt")
 
 
     println("\n***** End of theory *****")
