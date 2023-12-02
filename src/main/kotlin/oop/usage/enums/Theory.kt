8a8bfb9b577657853ea1d9879371a4ec161d3895
loicbaillif <larmedeciel@gmail.com>
2023-12-02 11:10:37 +0100

    Kotlin/begin/enum/th/v3 Second parameter

--- src/main/kotlin/oop/usage/enums/Theory.kt
+++ src/main/kotlin/oop/usage/enums/Theory.kt
@@ -18,14 +18,14 @@ fun theory() {
 }
 
 
-enum class Rainbow (val color: String) {
-    RED("Red"),
-    ORANGE("Orange"),
-    YELLOW("Yellow"),
-    GREEN("Green"),
-    BLUE("Blue"),
-    INDIGO("Indigo"),
-    VIOLET("Violet")
+enum class Rainbow (val color: String, val rgb: String) {
+    RED("Red", "#FF0000"),
+    ORANGE("Orange", "#FF7F00"),
+    YELLOW("Yellow", "#FFFF00"),
+    GREEN("Green", "#00FF00"),
+    BLUE("Blue", "#0000FF"),
+    INDIGO("Indigo", "#4B0082"),
+    VIOLET("Violet", "8B00FF")
 }
 
 
