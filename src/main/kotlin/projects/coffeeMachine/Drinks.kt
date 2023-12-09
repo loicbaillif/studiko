05639f506da866fa075171551a1cd82ccf2210cb
loicbaillif <larmedeciel@gmail.com>
2023-12-09 12:55:27 +0100

    Kotlin/begin/coffeeMachine/st6/v4 Add cups to Drinks

--- src/main/kotlin/projects/coffeeMachine/Drinks.kt
+++ src/main/kotlin/projects/coffeeMachine/Drinks.kt
@@ -1,7 +1,7 @@
 package projects.coffeeMachine
 
-enum class Drinks(val coffee: Int, val milk: Int, val water: Int, val cost: Int) {
-    CAPPUCCINO(-12, -100, -200, 6),
-    ESPRESSO(-16, 0, -250, 4),
-    LATTE(20, 75, 350, 7);
+enum class Drinks(val coffee: Int, val milk: Int, val water: Int, val cups: Int, val cost: Int) {
+    CAPPUCCINO(-12, -100, -200, -1, 6),
+    ESPRESSO(-16, 0, -250, -1, 4),
+    LATTE(20, 75, 350, -1, 7);
 }
