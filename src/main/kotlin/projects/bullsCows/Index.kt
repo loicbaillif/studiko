3adf13bfebd8345773a906e37644651971e88dd8
loicbaillif <larmedeciel@gmail.com>
2023-12-10 10:57:09 +0100

    Kotlin/begin/bullsCows/st1/init Bulls and Cows: Stage 1 - init

--- src/main/kotlin/projects/bullsCows/Index.kt
+++ src/main/kotlin/projects/bullsCows/Index.kt
@@ -3,7 +3,7 @@ package projects.bullsCows
 fun index() {
     println("********** PROJECT : BULLS AND COWS **********\n")
 
-
+    stage1()
 
     println("\n\n********** END OF PROJECT **********")
 }
---
+++ src/main/kotlin/projects/bullsCows/Stage1.kt
@@ -0,0 +1,12 @@
+package projects.bullsCows
+
+/**
+ * Stage 1/7 : Game log
+ * https://hyperskill.org/projects/364/stages/2155/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage1() {
+
+}
