2b871db0e6171c274b9304c9c694054142855e17
loicbaillif <larmedeciel@gmail.com>
2023-12-10 11:18:53 +0100

    Kotlin/begin/bullsCows/st1/v1 Print output ...

--- src/main/kotlin/projects/bullsCows/Stage1.kt
+++ src/main/kotlin/projects/bullsCows/Stage1.kt
@@ -8,5 +8,14 @@ package projects.bullsCows
  */
 
 fun stage1() {
+    println("""The secret code is prepared: ****.
 
+Turn 1. Answer:
+1234
+Grade: None.
+
+Turn 2. Answer:
+9876
+Grade: 4 bulls.
+Congrats! The secret code is 9876.""")
 }
