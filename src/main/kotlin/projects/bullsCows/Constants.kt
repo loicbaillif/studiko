4efef30a94d0176be7f965225a7746df492b52b3
loicbaillif <larmedeciel@gmail.com>
2023-12-13 12:07:13 +0100

    Kotlin/begin/bullsCows/st3/v3 Define constant for invalid user input

--- src/main/kotlin/projects/bullsCows/Constants.kt
+++ src/main/kotlin/projects/bullsCows/Constants.kt
@@ -1,4 +1,6 @@
 package projects.bullsCows
 
 // String constants
+const val INVALID_PASSWORD_LENGTH = "Error: can't generate a secret number " +
+        "with a length of %d because there aren't enough unique digits."
 const val RESULT = "Grade: %s. The secret code is %s"
