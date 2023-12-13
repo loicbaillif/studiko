eadb81fa45112f5b0443f60f2e3934dc3aec3488
loicbaillif <larmedeciel@gmail.com>
2023-12-13 11:29:08 +0100

    Kotlin/begin/bullsCows/st3/init Stage 3/7: init

--- src/main/kotlin/projects/bullsCows/Index.kt
+++ src/main/kotlin/projects/bullsCows/Index.kt
@@ -4,7 +4,8 @@ fun index() {
     println("********** PROJECT : BULLS AND COWS **********\n")
 
     // stage1()
-    stage2()
+    // stage2()
+    stage3()
 
     println("\n\n********** END OF PROJECT **********")
 }
---
+++ src/main/kotlin/projects/bullsCows/Stage3.kt
@@ -0,0 +1,12 @@
+package projects.bullsCows
+
+/**
+ * Stage 3/7: Secret Code
+ * https://hyperskill.org/projects/364/stages/2157/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage3() {
+
+}
