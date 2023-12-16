6bed68f693279aa023b837c484194dbd8badbd9e
loicbaillif <larmedeciel@gmail.com>
2023-12-16 11:35:54 +0100

    Kotlin/begin/bullsCows/st4/init Stage 4/7 : Game time!

---
+++ src/main/kotlin/projects/bullsCows/Stage4.kt
@@ -0,0 +1,12 @@
+package projects.bullsCows
+
+/**
+ * Stage 4/7 : Game Time!
+ * https://hyperskill.org/projects/364/stages/2158/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage4() {
+
+}
