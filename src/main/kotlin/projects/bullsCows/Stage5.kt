5195da5832d1ae064d3b1edc9e4903e9b1ac223d
loicbaillif <larmedeciel@gmail.com>
2023-12-17 09:19:11 +0100

    Kotlin/begin/bullsCows/st5/init Stage 5/7: Improve the code generator

---
+++ src/main/kotlin/projects/bullsCows/Stage5.kt
@@ -0,0 +1,12 @@
+package projects.bullsCows
+
+/**
+ * Stage 5/7: Improve the code generator
+ * https://hyperskill.org/projects/364/stages/2159/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage5() {
+
+}
