95cacd898968638c8366be933f67669951247585
loicbaillif <larmedeciel@gmail.com>
2023-12-17 12:36:43 +0100

    Kotlin/begin/bullsCows/st6/init Stage 6: New level - init

---
+++ src/main/kotlin/projects/bullsCows/Stage6.kt
@@ -0,0 +1,12 @@
+package projects.bullsCows
+
+/**
+ * Stage 6/7: New level
+ * https://hyperskill.org/projects/364/stages/2160/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage6() {
+
+}
