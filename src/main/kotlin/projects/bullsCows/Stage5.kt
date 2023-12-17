03018055a5238441323ebc1127c1d8dc44957f54
loicbaillif <larmedeciel@gmail.com>
2023-12-17 10:44:48 +0100

    Kotlin/begin/bullsCows/st5/v6 Define stage5(); first proposition

--- src/main/kotlin/projects/bullsCows/Stage5.kt
+++ src/main/kotlin/projects/bullsCows/Stage5.kt
@@ -1,5 +1,7 @@
 package projects.bullsCows
 
+import java.util.Random
+
 /**
  * Stage 5/7: Improve the code generator
  * https://hyperskill.org/projects/364/stages/2159/implement
@@ -8,5 +10,13 @@ package projects.bullsCows
  */
 
 fun stage5() {
+    println(GET_PASSWORD_LENGTH)
+    val codeLength = getPasswordLength()
+    if (codeLength == 0) return
 
+    println(INIT_GAME)
+    val solution = getCode(codeLength)
+    playTurn(solution)
+
+    println(END_GAME)
 }
