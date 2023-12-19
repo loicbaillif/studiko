7ad8a44a1ed3f67d064443417fc766c0d7b246b2
loicbaillif <larmedeciel@gmail.com>
2023-12-19 22:32:35 +0100

    Kotlin/begin/nullNonNull/ex1/init Null or not null - exercise: init

---
+++ src/main/kotlin/typesDataStruct/dataTypes/nullNonNull/Exercise1.kt
@@ -0,0 +1,16 @@
+package typesDataStruct.dataTypes.nullNonNull
+
+/**
+ * Exercise: Null or not null
+ * https://hyperskill.org/learn/step/20494
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise1() {
+    println("*** Exercise: Null or not null ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
--- src/main/kotlin/typesDataStruct/dataTypes/nullNonNull/Index.kt
+++ src/main/kotlin/typesDataStruct/dataTypes/nullNonNull/Index.kt
@@ -3,5 +3,6 @@ package typesDataStruct.dataTypes.nullNonNull
 fun index() {
     println("********** Nullable and non-nullable types **********\n")
 
-    theory()
+    // theory()
+    exercise1()
 }
