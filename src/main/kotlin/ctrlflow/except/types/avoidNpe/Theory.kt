340ae72f02642fc2682bbe1571f30ec73bcc3ba2
loicbaillif <larmedeciel@gmail.com>
2023-12-20 11:29:17 +0100

    Kotlin/begin/avoidNpe/th/v1 Elvis operator

--- src/main/kotlin/ctrlflow/except/types/avoidNpe/Theory.kt
+++ src/main/kotlin/ctrlflow/except/types/avoidNpe/Theory.kt
@@ -9,5 +9,11 @@ package ctrlflow.except.types.avoidNpe
 fun theory() {
     println("***** Theory *****")
 
+    println("\n*** 1) Elvis operator")
+    val name: String? = null
+    val length: Int? = name?.length
+    println(if (length != null) length else 0)
+    println(length ?: 0)
+
     println("\n***** End of Theory *****")
 }
