2bf4cdb45b47ca82d51aad8089b9a6aa89c1071b
loicbaillif <larmedeciel@gmail.com>
2024-01-01 18:58:08 +0100

    Kotlin/begin/secondConstructor/th/v5 Tests

--- src/main/kotlin/oop/basics/secondConstructor/Theory.kt
+++ src/main/kotlin/oop/basics/secondConstructor/Theory.kt
@@ -14,6 +14,10 @@ fun theory() {
     val size11 = Size1(3, 4)
     val size21 = Size1(5, 1)
 
+    println("\n*** 2) Multiple constructors")
+    val size31 = Size1(8)
+    val size41 = Size1(3.2, 5)
+
 
 
     println("\n***** End of theory *****")
@@ -42,10 +46,12 @@ class Size1 {
     constructor(_width: Int, _height: Double) {
         width = _width
         height = _height.toInt()
+        println("Height value converted from Double ... Possible loss of accuracy")
     }
 
     constructor(_width: Double, _height: Int) {
         width = _width.toInt()
         height = _height
+        println("Width value converted from Double ... Possible loss of accuracy")
     }
 }
