2f913f81a7ddbd0d8f62e1f420e0c4d0c27a9dc2
loicbaillif <larmedeciel@gmail.com>
2024-01-01 15:49:44 +0100

    Kotlin/begin/secondConstructor/th/v1 Class Size1 created

--- src/main/kotlin/oop/basics/secondConstructor/Theory.kt
+++ src/main/kotlin/oop/basics/secondConstructor/Theory.kt
@@ -9,7 +9,20 @@ package oop.basics.secondConstructor
 fun theory() {
     println("***** Theory *****")
 
+    println("\n*** 1) Custom Constructor ***")
+
 
 
     println("\n***** End of theory *****")
+}
+
+
+class Size1 {
+    var width: Int = 0
+    var height: Int = 0
+
+    constructor(_width: Int, _height: Int) {
+        width = _width
+        height = _height
+    }
 }
