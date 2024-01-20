3a981599b8987ae4b4a9368e2b11ad956f7c96de
loicbaillif <larmedeciel@gmail.com>
2024-01-20 15:01:33 +0100

    Kotlin/begin/scopes/th/v1 scopes from branching

--- src/main/kotlin/ctrlflow/func/scopes/Theory.kt
+++ src/main/kotlin/ctrlflow/func/scopes/Theory.kt
@@ -9,6 +9,22 @@ package ctrlflow.func.scopes
 fun theory() {
     println("***** Theory *****")
 
+    println("\n*** 1) Branching")
+    val outerScope = 10
+    if (outerScope > 0) {
+        val innerScope = outerScope * 2
+        println("outer scope = $outerScope")
+        println("inner scope = $innerScope")
+    } else {
+        println("outer scope = $outerScope")
+        // println("inner scope = $innerScope") // Unresolved reference
+    }
+
+
+    println("outer scope = $outerScope")
+    // println("inner scope = $innerScope") // Unresolved reference
+
+
 
 
     println("\n***** End of theory *****")
