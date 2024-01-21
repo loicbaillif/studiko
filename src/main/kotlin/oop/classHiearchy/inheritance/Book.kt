d48bf065f9e1fc0ae52e5b7fb0f7087dbb81231b
loicbaillif <larmedeciel@gmail.com>
2024-01-21 11:59:24 +0100

    Kotlin/begin/inheritance/th/v3 Extend Book class

--- src/main/kotlin/oop/classHiearchy/inheritance/Book.kt
+++ src/main/kotlin/oop/classHiearchy/inheritance/Book.kt
@@ -1,5 +1,13 @@
 package oop.classHiearchy.inheritance
 
-open class Book(val pages: Int, val author: String) {
+/**
+ * Book:
+ * pages: Number of pages for original edition
+ * author: who wrote it
+ * cost: price, in cents
+ */
+
+open class Book(val title: String, val pages: Int, val author: String, var cost: Int = 0) {
+    fun getFullInfo(): String = "$title, written by $author, costs ${cost / 100} euros and ${cost % 100} cents"
 
 }
