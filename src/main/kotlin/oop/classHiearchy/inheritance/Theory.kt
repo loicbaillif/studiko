92412319434243dc1604f0ba90f40c4b60488c2f
loicbaillif <larmedeciel@gmail.com>
2024-01-21 11:55:36 +0100

    Kotlin/begin/inheritance/th/v2 Create first book object

--- src/main/kotlin/oop/classHiearchy/inheritance/Theory.kt
+++ src/main/kotlin/oop/classHiearchy/inheritance/Theory.kt
@@ -10,7 +10,7 @@ fun theory() {
     println("***** Theory *****")
 
     println("\n*** 1) Open the class")
-
+    val germinal = Book(435, "Emile Zola")
 
     println("\n***** End of theory *****")
 }
