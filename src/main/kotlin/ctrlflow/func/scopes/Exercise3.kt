70c126c9ca21de045516cec98f1beaf21a422260
loicbaillif <larmedeciel@gmail.com>
2024-01-21 10:14:03 +0100

    Kotlin/begin/scopes/ex3/v1 Bugged version

--- src/main/kotlin/ctrlflow/func/scopes/Exercise3.kt
+++ src/main/kotlin/ctrlflow/func/scopes/Exercise3.kt
@@ -10,7 +10,11 @@ package ctrlflow.func.scopes
 fun exercise3() {
     println("*** Exercise: Power and Scopes ***")
 
-
+    for (i in 2..10) {
+        var thirteen = 13L
+        thirteen *= 13
+        println(thirteen)
+    }
 
     println("\n*** End of exercise ***")
 }
