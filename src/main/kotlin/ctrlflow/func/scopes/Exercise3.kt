722bbbfcd0cc94a9eb4523f4a6dbc7ba99514308
loicbaillif <larmedeciel@gmail.com>
2024-01-21 10:12:54 +0100

    Kotlin/begin/scopes/ex3/init Ex3 - init: Power and scopes

---
+++ src/main/kotlin/ctrlflow/func/scopes/Exercise3.kt
@@ -0,0 +1,16 @@
+package ctrlflow.func.scopes
+
+/**
+ * Exercise: Power and scopes
+ * https://hyperskill.org/learn/step/17121
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun exercise3() {
+    println("*** Exercise: Power and Scopes ***")
+
+
+
+    println("\n*** End of exercise ***")
+}
