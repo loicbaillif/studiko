48b4112b8416e46d551ab7685adeac90a347f490
loicbaillif <larmedeciel@gmail.com>
2024-01-21 12:22:03 +0100

    Kotlin/begin/inheritance/th/v9 Add functions to ComicsTh

--- src/main/kotlin/oop/classHiearchy/inheritance/ComicsTh.kt
+++ src/main/kotlin/oop/classHiearchy/inheritance/ComicsTh.kt
@@ -1,4 +1,13 @@
 package oop.classHiearchy.inheritance
 
 class ComicsTh(title: String, pages: Int, author: String, cost: Int) : BookTh(title, pages, author, cost) {
+    fun getEurCost(): String {
+        return "${cost / 100}€${cost % 100}"
+    }
+
+    fun getUsdCost(): String {
+        val rateEurUsd = 1.21
+        val usCost = cost * rateEurUsd
+        return "${usCost / 100}$${usCost % 100}"
+    }
 }
