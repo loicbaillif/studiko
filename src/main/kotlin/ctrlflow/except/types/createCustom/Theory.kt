0cb41aef88478c152ff2aea90604132e81365ca8
loicbaillif <larmedeciel@gmail.com>
2024-01-28 11:59:16 +0100

    Kotlin/begin/customExcept/th/v2 Define testFunTh()

--- src/main/kotlin/ctrlflow/except/types/createCustom/Theory.kt
+++ src/main/kotlin/ctrlflow/except/types/createCustom/Theory.kt
@@ -10,6 +10,8 @@ fun theory() {
     println("***** Theory *****")
 
     println("\n*** 1) Throwing an exception")
+
+    /*
     println("Do you want an explicit exception?\n\t1 = true\t0 = false")
     val userRequest = readln().toInt()
     if (userRequest == 0) {
@@ -17,6 +19,15 @@ fun theory() {
     } else {
         throw Exception("This is the reason why.")
     }
+     */
+
+
+
 
     println("\n***** End of theory *****")
+}
+
+
+fun testFunTh(a: Int, b: Int): Int {
+    return a / b
 }
