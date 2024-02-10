cf8fc565244a188d97433415f5a5a420604be1f1
loicbaillif <larmedeciel@gmail.com>
2024-02-10 18:34:12 +0100

    Kotlin/begin/unitConv/st1/init Stage 1/5: Init

--- src/main/kotlin/projects/unitConverter/Index.kt
+++ src/main/kotlin/projects/unitConverter/Index.kt
@@ -3,7 +3,7 @@ package projects.unitConverter
 fun index() {
     println("********** PROJECT : UNIT CONVERTER **********\n")
 
-
+    stage1()
 
     println("\n\n********** END OF PROJECT **********")
 }
---
+++ src/main/kotlin/projects/unitConverter/Stage1.kt
@@ -0,0 +1,12 @@
+package projects.unitConverter
+
+/**
+ * Stage 1/5: Standard Output
+ * https://hyperskill.org/projects/70/stages/385/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage1() {
+
+}
