7ea871a9f6b8958ac6d473f3dcc23d93a15da589
loicbaillif <larmedeciel@gmail.com>
2024-02-10 18:48:03 +0100

    Kotlin/begin/unitConv/st2/init Stage 2/5: Let's get metric

--- src/main/kotlin/projects/unitConverter/Index.kt
+++ src/main/kotlin/projects/unitConverter/Index.kt
@@ -3,7 +3,8 @@ package projects.unitConverter
 fun index() {
     println("********** PROJECT : UNIT CONVERTER **********\n")
 
-    stage1()
+    // stage1()
+    stage2()
 
     println("\n\n********** END OF PROJECT **********")
 }
---
+++ src/main/kotlin/projects/unitConverter/Stage2.kt
@@ -0,0 +1,12 @@
+package projects.unitConverter
+
+/**
+ * Stage 2/5: Let's get metric
+ * https://hyperskill.org/projects/70/stages/386/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage2() {
+
+}
