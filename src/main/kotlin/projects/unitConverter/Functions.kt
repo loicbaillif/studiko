25fe20bc7fa95f3c7a1c7eec515159dae95931f3
loicbaillif <larmedeciel@gmail.com>
2024-02-10 19:14:57 +0100

    Kotlin/begin/unitConv/st2/v4 Split input

--- src/main/kotlin/projects/unitConverter/Functions.kt
+++ src/main/kotlin/projects/unitConverter/Functions.kt
@@ -3,6 +3,7 @@ package projects.unitConverter
 fun getUserInput(): List<String> {
     println(INPUT_PROMPT)
     val userInput = readln()
+    val result = userInput.lowercase().split(" ");
 
     return listOf("0", "0")
 }
