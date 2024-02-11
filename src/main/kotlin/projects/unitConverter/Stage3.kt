ac2be5b109d221afc5fd0e8cc9d2a79829786b5b
loicbaillif <larmedeciel@gmail.com>
2024-02-11 07:32:03 +0100

    Kotlin/begin/unitConv/st3/init Stage 3/5 : Distances

---
+++ src/main/kotlin/projects/unitConverter/Stage3.kt
@@ -0,0 +1,12 @@
+package projects.unitConverter
+
+/**
+ * Stage 3/5: Distances
+ * https://hyperskill.org/projects/70/stages/386/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage3() {
+
+}
