b0ae176d6a965d6821d2aa7116ab4623c6b9613c
loicbaillif <larmedeciel@gmail.com>
2024-02-12 21:24:23 +0100

    Kotlin/begin/unitConv/st4/init Stage 4/5: Heavy Duty

---
+++ src/main/kotlin/projects/unitConverter/Stage4.kt
@@ -0,0 +1,12 @@
+package projects.unitConverter
+
+/**
+ * Stage 4/5: Heavy duty
+ * https://hyperskill.org/projects/70/stages/388/implement
+ * @author JetBrains Academy
+ * Solution by Loïc Baillif
+ */
+
+fun stage4() {
+
+}
