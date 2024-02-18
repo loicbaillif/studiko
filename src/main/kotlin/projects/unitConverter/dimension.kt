28bdc933595196c97b75a93578bddfece4c7c6fb
loicbaillif <larmedeciel@gmail.com>
2024-02-18 11:21:50 +0100

    Kotlin/begin/unitConv/st4/v16 upgrade to get source type

--- src/main/kotlin/projects/unitConverter/dimension.kt
+++ src/main/kotlin/projects/unitConverter/dimension.kt
@@ -3,30 +3,29 @@ package projects.unitConverter
 class dimension(userInput: Array<String>) {
     val sourceDimension = userInput[1];
     val sourceFigure: Double = userInput[0].toDouble();
+    var sourceType = "";
     var dimensionPlural = "";
     var dimensionShort = "";
     var dimensionSingular = "";
-    val distancesList = arrayOf(distCm, distFt, distIn, distKm, distM, distMi, distMm, distYd)
-    val weightsList = arrayOf(weightG, weightKg, weightLb, weightMg, weightOz)
+    val distancesList = arrayOf(distM, distCm, distFt, distIn, distKm, distMi, distMm, distYd)
+    val weightsList = arrayOf(weightKg, weightG, weightLb, weightMg, weightOz)
+    val unitsList = arrayOf(distancesList, weightsList)
+    val unitsTypes = arrayOf("distance", "weight")
+    var typeCounter = 0
 
     init {
-        for (elt in distancesList) {
-            if (elt.indexOf(sourceDimension) != -1) {
-                // unit is a distance
-                dimensionPlural = elt[2]
-                dimensionShort = elt[0]
-                dimensionSingular = elt[1]
-                break
+        for (unit in unitsList) {
+            for (elt in unit) {
+                if (elt.indexOf(sourceDimension) != -1) {
+                    dimensionPlural = elt[2]
+                    dimensionShort = elt[0]
+                    dimensionSingular = elt[1]
+                    sourceType = unitsTypes[typeCounter]
+                    break
+                }
             }
-        }
-
-        for (elt in weightsList) {
-            if (elt.indexOf(sourceDimension) != -1) {
-                // Unit is a weight
-                dimensionPlural = elt[2]
-                dimensionShort = elt[0]
-                dimensionSingular = elt[1]
-            }
+            if (sourceType != "") break
+            typeCounter++
         }
     }
 
