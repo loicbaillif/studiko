6201d3afd8da5a207ad8f990a043da41089da5fd
loicbaillif <larmedeciel@gmail.com>
2024-02-18 10:37:23 +0100

    Kotlin/begin/unitConv/st4/v14 Define init

--- src/main/kotlin/projects/unitConverter/dimension.kt
+++ src/main/kotlin/projects/unitConverter/dimension.kt
@@ -6,5 +6,26 @@ class dimension(userInput: Array<String>) {
     var dimensionShort = "";
     var dimensionSingular = "";
     val distancesList = arrayOf(distCm, distFt, distIn, distKm, distM, distMi, distMm, distYd)
-    val weightList = arrayOf(weightG, weightKg, weightLb, weightMg, weightOz)
+    val weightsList = arrayOf(weightG, weightKg, weightLb, weightMg, weightOz)
+
+    init {
+        for (elt in distancesList) {
+            if (elt.indexOf(sourceDimension) != -1) {
+                // unit is a distance
+                dimensionPlural = elt[2]
+                dimensionShort = elt[0]
+                dimensionSingular = elt[1]
+                break
+            }
+        }
+
+        for (elt in weightsList) {
+            if (elt.indexOf(sourceDimension) != -1) {
+                // Unit is a weight
+                dimensionPlural = elt[2]
+                dimensionShort = elt[0]
+                dimensionSingular = elt[1]
+            }
+        }
+    }
 }
